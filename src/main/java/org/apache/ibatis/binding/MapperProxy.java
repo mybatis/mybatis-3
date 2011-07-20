9b34e7aa8c644593c1ea84a81b483cde24d0f6fd
Eduardo Macarron <eduardo.macarron@gmail.com>
2011-07-20 21:01:59 +0000

    Fix for http://code.google.com/p/mybatis/issues/detail?id=362 . Mappers should return a not null value from toString(), hashCode() and equals()

--- src/main/java/org/apache/ibatis/binding/MapperProxy.java
+++ src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -4,27 +4,12 @@ import java.io.Serializable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.apache.ibatis.session.SqlSession;
 
 public class MapperProxy implements InvocationHandler, Serializable {
 
   private static final long serialVersionUID = -6424540398559729838L;
-
-  private static final Set<String> OBJECT_METHODS = new HashSet<String>() {
-    private static final long serialVersionUID = -1782950882770203582L;
-  {
-    add("toString");
-    add("getClass");
-    add("hashCode");
-    add("equals");
-    add("wait");
-    add("notify");
-    add("notifyAll");
-  }};
-
   private SqlSession sqlSession;
 
   private <T> MapperProxy(SqlSession sqlSession) {
@@ -32,7 +17,7 @@ public class MapperProxy implements InvocationHandler, Serializable {
   }
 
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    if (!OBJECT_METHODS.contains(method.getName())) {
+    if (method.getDeclaringClass() != Object.class) {
       final Class<?> declaringInterface = findDeclaringInterface(proxy, method);
       final MapperMethod mapperMethod = new MapperMethod(declaringInterface, method, sqlSession);
       final Object result = mapperMethod.execute(args);
@@ -40,8 +25,9 @@ public class MapperProxy implements InvocationHandler, Serializable {
         throw new BindingException("Mapper method '" + method.getName() + "' (" + method.getDeclaringClass() + ") attempted to return null from a method with a primitive return type (" + method.getReturnType() + ").");
       }
       return result;
+    } else {
+      return method.invoke(this, args);
     }
-    return null;
   }
 
   private Class<?> findDeclaringInterface(Object proxy, Method method) {
@@ -67,7 +53,7 @@ public class MapperProxy implements InvocationHandler, Serializable {
   }
 
   @SuppressWarnings("unchecked")
-public static <T> T newMapperProxy(Class<T> mapperInterface, SqlSession sqlSession) {
+  public static <T> T newMapperProxy(Class<T> mapperInterface, SqlSession sqlSession) {
     ClassLoader classLoader = mapperInterface.getClassLoader();
     Class<?>[] interfaces = new Class[]{mapperInterface};
     MapperProxy proxy = new MapperProxy(sqlSession);
