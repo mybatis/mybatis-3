1aaad4ec87608901b8033edfb90011b2346f0783
Simone Tripodi <simone.tripodi@gmail.com>
2011-11-11 16:37:12 +0000

    better looking DB types/names registration

--- src/main/java/org/apache/ibatis/jdbc/DataSourceUtils.java
+++ src/main/java/org/apache/ibatis/jdbc/DataSourceUtils.java
@@ -1,9 +1,10 @@
 package org.apache.ibatis.jdbc;
 
+import static java.util.Arrays.asList;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,53 +28,49 @@ public class DataSourceUtils {
   static {
     // data kindly borrowed from DbVisualizer (http://www.dbvis.com/)
     register("cache", "Cache");
-    register("db2", "DB2");
-    register("db2", "DB2 (DataDirect)");
-    register("generic", "DB2 for AS/400 (JTOpen)");
+    register("db2", "DB2",
+                    "DB2 (DataDirect)");
+    register("generic", "DB2 for AS/400 (JTOpen)",
+                        "JDBC/ODBC Bridge", "McKoi");
     register("firebird", "Firebird");
     register("frontbase", "FrontBase");
     register("neoview", "HP Neoview");
-    register("hsql", "HSQLDB server");
-    register("hsql", "HSQLDB embedded");
-    register("h2", "H2 server");
-    register("h2", "H2 embedded");
-    register("informix", "Informix");
-    register("informix", "Informix (DataDirect)");
-    register("derby", "JavaDB/Derby server");
-    register("derby", "JavaDB/Derby embedded");
+    register("hsql", "HSQLDB server",
+                     "HSQLDB embedded");
+    register("h2", "H2 server",
+                   "H2 embedded");
+    register("informix", "Informix",
+                         "Informix (DataDirect)");
+    register("derby", "JavaDB/Derby server",
+                      "JavaDB/Derby embedded");
     register("jdatastore", "JDataStore");
-    register("generic", "JDBC/ODBC Bridge");
     register("maxdb", "MaxDB");
-    register("generic", "McKoi");
     register("mimer", "Mimer");
     register("mysql", "MySQL");
     register("netezza", "Netezza");
-    register("oracle", "Oracle Thin");
-    register("oracle", "Oracle OCI");
-    register("oracle", "Oracle (DataDirect)");
+    register("oracle", "Oracle Thin",
+                       "Oracle OCI",
+                       "Oracle (DataDirect)");
     register("pervasive", "Pervasive");
     register("postgresql", "PostgreSQL");
     register("progress", "Progress");
     register("sqlite", "SQLite");
-    register("sqlserver", "SQL Server (DataDirect)");
-    register("sqlserver", "SQL Server (jTDS)");
-    register("sqlserver", "SQL Server (Microsoft JDBC Driver)");
-    register("sqlserver", "SQL Server 2008 (Microsoft JDBC Driver)");
-    register("sybase-ase", "Sybase ASE (jTDS)");
-    register("sybase-ase", "Sybase ASE (JConnect)");
-    register("sybase-asa", "Sybase SQL Anywhere (JConnect)");
+    register("sqlserver", "SQL Server (DataDirect)",
+                          "SQL Server (jTDS)",
+                          "SQL Server (Microsoft JDBC Driver)",
+                          "SQL Server 2008 (Microsoft JDBC Driver)" );
+    register("sybase-ase", "Sybase ASE (jTDS)",
+                           "Sybase ASE (JConnect)",
+                           "Sybase SQL Anywhere (JConnect)");
     register("sybase", "Sybase (DataDirect)");
   }
 
-  private static void register(String type, String databaseProductName) {
-    Collection<String> names = TYPE_NAME.get(type);
-    if (names == null) {
-      names = new ArrayList<String>();
-      TYPE_NAME.put(type, names);
-    }
-    TYPE_NAME.put(type, names);
+  private static void register(String type, String...databaseProductNames) {
+    TYPE_NAME.put(type, asList(databaseProductNames));
 
-    NAME_TYPE.put(databaseProductName, type);
+    for (String databaseProductName : databaseProductNames) {
+      NAME_TYPE.put(databaseProductName, type);
+    }
   }
 
   public static String getDatabaseName(DataSource dataSource) {
