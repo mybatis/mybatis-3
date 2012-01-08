aae6971e2aeecd7b8d42ab65680c1d410e49b72a
Eduardo Macarron <eduardo.macarron@gmail.com>
2012-01-08 08:50:23 +0000

    failing test for http://code.google.com/p/mybatis/issues/detail?id=165  (ignored)

---
+++ src/test/java/org/apache/ibatis/submitted/valueinmap/ValueInMapTest.java
@@ -0,0 +1,94 @@
+/*
+ *    Copyright 2009-2011 The MyBatis Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.submitted.valueinmap;
+
+import static org.junit.Assert.*;
+
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ValueInMapTest {
+
+  protected static SqlSessionFactory sqlSessionFactory;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    Connection conn = null;
+
+    // save charset
+    Charset charset = Resources.getCharset();
+
+    try {
+      Class.forName("org.hsqldb.jdbcDriver");
+      conn = DriverManager.getConnection("jdbc:hsqldb:mem:encoding", "sa", "");
+
+      // make sure that the SQL file has been saved in UTF-8!
+      Resources.setCharset(Charset.forName("utf-8"));
+      Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/valueinmap/CreateDB.sql");
+
+      ScriptRunner runner = new ScriptRunner(conn);
+      runner.setLogWriter(null);
+      runner.setErrorLogWriter(null);
+      runner.runScript(reader);
+      conn.commit();
+      reader.close();
+
+      reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/valueinmap/mybatis-config.xml");
+      sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+      reader.close();
+
+    } finally {
+      // restore charset
+      Resources.setCharset(charset);
+      
+      if (conn != null) {
+        conn.close();
+      }
+    }
+  }
+
+  @Ignore // see issue 165
+  @Test
+  public void testEncoding2() {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      
+      Map<String, String> map = new HashMap<String, String>();
+      map.put("table", "names");
+      map.put("column", "id");
+      map.put("value", "1");
+      
+      int count = sqlSession.selectOne("count", map);
+      assertTrue(count == 1);
+      
+    } finally {
+      sqlSession.close();
+    }
+  }
+}
