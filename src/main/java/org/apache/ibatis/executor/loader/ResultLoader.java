bd836e13941320ee23ba745511e0271425ab6f8b
Eduardo Macarron <eduardo.macarron@gmail.com>
2012-02-04 16:06:16 +0000

    always use ObjectBuilder to create new objects (copy&paste from MapperMethod)

--- src/main/java/org/apache/ibatis/executor/loader/ResultLoader.java
+++ src/main/java/org/apache/ibatis/executor/loader/ResultLoader.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009-2011 The MyBatis Team
+ *    Copyright 2009-2012 The MyBatis Team
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,11 +17,7 @@ package org.apache.ibatis.executor.loader;
 
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import javax.sql.DataSource;
 
@@ -36,9 +32,6 @@ import org.apache.ibatis.transaction.Transaction;
 import org.apache.ibatis.transaction.TransactionFactory;
 
 public class ResultLoader {
-
-  protected static final Class<?>[] LIST_INTERFACES = new Class<?>[]{List.class};
-  protected static final Class<?>[] SET_INTERFACES = new Class<?>[]{Set.class};
 
   protected final Configuration configuration;
   protected final Executor executor;
@@ -57,14 +50,12 @@ public class ResultLoader {
     this.targetType = targetType;
   }
 
-  public Object loadResult() throws SQLException {
-    List<Object> list = selectList();
-    if (targetType != null && SortedSet.class.isAssignableFrom(targetType)) { // issue #426 support TreeSet
-      resultObject = new TreeSet<Object>(list);
-    } else if (targetType != null && Set.class.isAssignableFrom(targetType)) {
-      resultObject = new HashSet<Object>(list);
-    } else if (targetType != null && Collection.class.isAssignableFrom(targetType)) {
+  public <E> Object loadResult() throws SQLException {
+    List<E> list = selectList();
+    if (targetType != null && targetType.isAssignableFrom(list.getClass())) {
       resultObject = list;
+    } else if (targetType != null && Collection.class.isAssignableFrom(targetType)) {
+      resultObject = convertToDeclaredCollection(list, targetType);
     } else if (targetType != null && targetType.isArray()) {
       resultObject = listToArray(list, targetType.getComponentType());
     } else {
@@ -83,7 +74,7 @@ public class ResultLoader {
       localExecutor = newExecutor();
     }
     try {
-      return localExecutor.<E>query(mappedStatement, parameterObject, RowBounds.DEFAULT, Executor.NO_RESULT_HANDLER);
+      return localExecutor.<E> query(mappedStatement, parameterObject, RowBounds.DEFAULT, Executor.NO_RESULT_HANDLER);
     } finally {
       if (localExecutor != executor) {
         localExecutor.close(false);
@@ -93,8 +84,7 @@ public class ResultLoader {
 
   private Executor newExecutor() throws SQLException {
     Environment environment = configuration.getEnvironment();
-    if (environment == null)
-      throw new ExecutorException("ResultLoader could not load lazily.  Environment was not configured.");
+    if (environment == null) throw new ExecutorException("ResultLoader could not load lazily.  Environment was not configured.");
     DataSource ds = environment.getDataSource();
     if (ds == null) throw new ExecutorException("ResultLoader could not load lazily.  DataSource was not configured.");
     final TransactionFactory transactionFactory = environment.getTransactionFactory();
@@ -106,10 +96,18 @@ public class ResultLoader {
     return resultObject == null;
   }
 
-  private Object[] listToArray(List<Object> list, Class<?> type) {
-    Object array = java.lang.reflect.Array.newInstance(type, list.size());
-    array = list.toArray((Object[]) array);
-    return (Object[]) array;
+  @SuppressWarnings("unchecked")
+  private <E> E[] listToArray(List<E> list, Class<?> type) {
+    E[] array = (E[]) java.lang.reflect.Array.newInstance(type, list.size());
+    array = list.toArray(array);
+    return array;
+  }
+
+  @SuppressWarnings("unchecked")
+  private <E> Collection<E> convertToDeclaredCollection(List<E> result, Class<?> type) {
+    Collection<E> collection = (Collection<E>) configuration.getObjectFactory().create(type);
+    collection.addAll(result);
+    return collection;
   }
 
 }
