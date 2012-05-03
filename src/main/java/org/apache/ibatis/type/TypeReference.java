adfddcaad3d35443dd5410db99f0d68af9d5a4cd
Simone Tripodi <simone.tripodi@gmail.com>
2012-05-03 08:58:25 +0000

    [581] extending StringTypeHandler throws TypeException

--- src/main/java/org/apache/ibatis/type/TypeReference.java
+++ src/main/java/org/apache/ibatis/type/TypeReference.java
@@ -26,19 +26,31 @@ import java.lang.reflect.Type;
  */
 public abstract class TypeReference<T> {
 
-  private Type rawType;
+  private final Type rawType;
 
   protected TypeReference() {
-    Type superclass = getClass().getGenericSuperclass();
-    if (superclass instanceof Class) {
+    rawType = getSuperclassTypeParameter(getClass());
+  }
+
+  Type getSuperclassTypeParameter(Class<?> clazz) {
+    Type genericSuperclass = clazz.getGenericSuperclass();
+    if (genericSuperclass instanceof Class) {
+      // try to climb up the hierarchy until meet something useful
+      if (TypeReference.class != genericSuperclass) {
+        return getSuperclassTypeParameter(clazz.getSuperclass());
+      }
+
       throw new TypeException("'" + getClass() + "' extends TypeReference but misses the type parameter. "
         + "Remove the extension or add a type parameter to it.");
     }
-    rawType = ((ParameterizedType) superclass).getActualTypeArguments()[0];
+
+    Type rawType = ((ParameterizedType) genericSuperclass).getActualTypeArguments()[0];
     // TODO remove this when Reflector is fixed to return Types
     if (rawType instanceof ParameterizedType) {
       rawType = ((ParameterizedType) rawType).getRawType();
     }
+
+    return rawType;
   }
 
   public final Type getRawType() {
