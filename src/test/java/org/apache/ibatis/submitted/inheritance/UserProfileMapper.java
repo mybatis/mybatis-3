85d2dab7b80d59c540618ee3d7fd4c03d159b4c2
Eduardo Macarron <eduardo.macarron@gmail.com>
2012-07-20 18:11:15 +0000

    remove uneeded @Overrides

--- src/test/java/org/apache/ibatis/submitted/inheritance/UserProfileMapper.java
+++ src/test/java/org/apache/ibatis/submitted/inheritance/UserProfileMapper.java
@@ -3,7 +3,7 @@ package org.apache.ibatis.submitted.inheritance;
 import org.apache.ibatis.annotations.Select;
 
 public interface UserProfileMapper extends BaseMapper<UserProfile> {
-  @Override
+
   @Select("select * from user_profile")
   UserProfile retrieveById(Integer id);
 }
