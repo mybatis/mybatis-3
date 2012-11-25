b1450c91a4c928a28e52749ea82d28b5ce216236
Eduardo Macarron <eduardo.macarron@gmail.com>
2012-11-25 08:49:16 +0000

    http://code.google.com/p/mybatis/issues/detail?id=719 :	NPE for NULL columns handled by ArrayTypeHandler

--- src/main/java/org/apache/ibatis/type/ArrayTypeHandler.java
+++ src/main/java/org/apache/ibatis/type/ArrayTypeHandler.java
@@ -23,34 +23,31 @@ import java.sql.SQLException;
 
 public class ArrayTypeHandler extends BaseTypeHandler<Object> {
 
-    public ArrayTypeHandler() {
-        super();
-    }
+  public ArrayTypeHandler() {
+    super();
+  }
 
-    @Override
-    public void setNonNullParameter(PreparedStatement ps, int i,
-            Object parameter, JdbcType jdbcType) throws SQLException {
-        ps.setArray(i, (Array) parameter);
-    }
+  @Override
+  public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+    ps.setArray(i, (Array) parameter);
+  }
 
-    @Override
-    public Object getNullableResult(ResultSet rs, String columnName)
-            throws SQLException {
-        Array array = rs.getArray(columnName);
-        return array.getArray();
-    }
+  @Override
+  public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
+    Array array = rs.getArray(columnName);
+    return array == null ? null : array.getArray();
+  }
 
-    @Override
-    public Object getNullableResult(ResultSet rs, int columnIndex)
-            throws SQLException {
-        Array array = rs.getArray(columnIndex);
-        return array.getArray();
-    }
+  @Override
+  public Object getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+    Array array = rs.getArray(columnIndex);
+    return array == null ? null : array.getArray();
+  }
 
-    @Override
-    public Object getNullableResult(CallableStatement cs, int columnIndex)
-            throws SQLException {
-        Array array = cs.getArray(columnIndex);
-        return array.getArray();
-    }
+  @Override
+  public Object getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+    Array array = cs.getArray(columnIndex);
+    return array == null ? null : array.getArray();
+  }
+
 }
