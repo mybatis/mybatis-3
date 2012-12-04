2219e7d96f8b4eb946027442b2df98dbfd735878
Eduardo Macarron <eduardo.macarron@gmail.com>
2012-12-04 05:39:59 +0000

    defaultStatementTimeout samples to 25 (not 25000)

--- src/test/java/org/apache/ibatis/submitted/lazyload_proxyfactory_comparison/JavassistLazyTest.java
+++ src/test/java/org/apache/ibatis/submitted/lazyload_proxyfactory_comparison/JavassistLazyTest.java
@@ -17,7 +17,7 @@ package org.apache.ibatis.submitted.lazyload_proxyfactory_comparison;
 
 import org.junit.Ignore;
 
-@Ignore("See Issue 664: Javassist ProxyFactory does not handle interfaces with generics correctly.")
+//@Ignore("See Issue 664: Javassist ProxyFactory does not handle interfaces with generics correctly.")
 public class JavassistLazyTest 
 extends AbstractLazyTest {
 	@Override
