2d18c92e05bcd008455b66b12485cafa1cc65abb
Eduardo Macarron <eduardo.macarron@gmail.com>
2013-01-05 10:48:49 +0000

    Locale use consistence

--- src/main/java/org/apache/ibatis/reflection/property/PropertyNamer.java
+++ src/main/java/org/apache/ibatis/reflection/property/PropertyNamer.java
@@ -31,7 +31,7 @@ public class PropertyNamer {
     }
 
     if (name.length() == 1 || (name.length() > 1 && !Character.isUpperCase(name.charAt(1)))) {
-      name = name.substring(0, 1).toLowerCase(Locale.US) + name.substring(1);
+      name = name.substring(0, 1).toLowerCase(Locale.ENGLISH) + name.substring(1);
     }
 
     return name;
