eab15c5e16ba4ada19bb638f722e34bd071c9567
Eduardo Macarron <eduardo.macarron@gmail.com>
2013-01-15 22:23:53 +0000

    test fixed

--- src/test/java/org/apache/ibatis/logging/jdbc/ResultSetLoggerTest.java
+++ src/test/java/org/apache/ibatis/logging/jdbc/ResultSetLoggerTest.java
@@ -47,7 +47,7 @@ public class ResultSetLoggerTest {
     when(rs.getMetaData()).thenReturn(metaData);
     when(metaData.getColumnCount()).thenReturn(1);
     when(metaData.getColumnType(1)).thenReturn(type);
-    when(metaData.getColumnName(1)).thenReturn("ColumnName");
+    when(metaData.getColumnLabel(1)).thenReturn("ColumnName");
     when(rs.getString(1)).thenReturn("value");
     when(log.isTraceEnabled()).thenReturn(true);
     ResultSet resultSet = ResultSetLogger.newInstance(rs, log);
