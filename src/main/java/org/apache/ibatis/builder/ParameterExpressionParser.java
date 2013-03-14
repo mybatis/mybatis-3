9cd2a7d302157e2886b8eacc25615465b92deb0a
Iwao AVE! <harawata@gmail.com>
2013-03-14 17:46:02 +0900

    Memory usage optimization.

--- src/main/java/org/apache/ibatis/builder/ParameterExpressionParser.java
+++ src/main/java/org/apache/ibatis/builder/ParameterExpressionParser.java
@@ -34,67 +34,67 @@ public class ParameterExpressionParser {
 
   public static Map<String, String> parse(String expression) {
     Map<String, String> map = new HashMap<String, String>();
-    parse(expression.toCharArray(), map);
+    parse(expression, map);
     return map;
   }
 
-  private static void parse(char[] expression, Map<String, String> map) {
+  private static void parse(String expression, Map<String, String> map) {
     int p = skipWS(expression, 0);
-    if (expression[p] == '(') {
+    if (expression.charAt(p) == '(') {
       expression(expression, p + 1, map);
     } else {
       property(expression, p, map);
     }
   }
 
-  private static void expression(char[] expression, int left, Map<String, String> map) {
+  private static void expression(String expression, int left, Map<String, String> map) {
     int match = 1;
     int right = left + 1;
     while (match > 0) {
-      if (expression[right] == ')') {
+      if (expression.charAt(right) == ')') {
         match--;
-      } else if (expression[right] == '(') {
+      } else if (expression.charAt(right) == '(') {
         match++;
       }
       right++;
     }
-    map.put("expression", new String(expression, left, right - left - 1));
+    map.put("expression", expression.substring(left, right - 1));
     jdbcTypeOpt(expression, right, map);
   }
 
-  private static void property(char[] expression, int left, Map<String, String> map) {
-    if (left < expression.length) {
+  private static void property(String expression, int left, Map<String, String> map) {
+    if (left < expression.length()) {
       int right = skipUntil(expression, left, ",:");
       map.put("property", trimmedStr(expression, left, right));
       jdbcTypeOpt(expression, right, map);
     }
   }
 
-  private static int skipWS(char[] expression, int p) {
-    for (int i = p; i < expression.length; i++) {
-      if (expression[i] > 0x20) {
+  private static int skipWS(String expression, int p) {
+    for (int i = p; i < expression.length(); i++) {
+      if (expression.charAt(i) > 0x20) {
         return i;
       }
     }
-    return expression.length;
+    return expression.length();
   }
 
-  private static int skipUntil(char[] expression, int p, final String endChars) {
-    for (int i = p; i < expression.length; i++) {
-      char c = expression[i];
+  private static int skipUntil(String expression, int p, final String endChars) {
+    for (int i = p; i < expression.length(); i++) {
+      char c = expression.charAt(i);
       if (endChars.indexOf(c) > -1) {
         return i;
       }
     }
-    return expression.length;
+    return expression.length();
   }
 
-  private static void jdbcTypeOpt(char[] expression, int p, Map<String, String> map) {
+  private static void jdbcTypeOpt(String expression, int p, Map<String, String> map) {
     p = skipWS(expression, p);
-    if (p < expression.length) {
-      if (expression[p] == ':') {
+    if (p < expression.length()) {
+      if (expression.charAt(p) == ':') {
         jdbcType(expression, p + 1, map);
-      } else if (expression[p] == ',') {
+      } else if (expression.charAt(p) == ',') {
         option(expression, p + 1, map);
       } else {
         throw new BuilderException("Parsing error in {" + new String(expression) + "} in position " + p);
@@ -102,7 +102,7 @@ public class ParameterExpressionParser {
     }
   }
 
-  private static void jdbcType(char[] expression, int p, Map<String, String> map) {
+  private static void jdbcType(String expression, int p, Map<String, String> map) {
     int left = skipWS(expression, p);
     int right = skipUntil(expression, left, ",");
     if (right > left) {
@@ -113,9 +113,9 @@ public class ParameterExpressionParser {
     option(expression, right + 1, map);
   }
 
-  private static void option(char[] expression, int p, Map<String, String> map) {
+  private static void option(String expression, int p, Map<String, String> map) {
     int left = skipWS(expression, p);
-    if (left < expression.length) {
+    if (left < expression.length()) {
       int right = skipUntil(expression, left, "=");
       String name = trimmedStr(expression, left, right);
       left = right + 1;
@@ -126,16 +126,16 @@ public class ParameterExpressionParser {
     }
   }
 
-  private static String trimmedStr(char[] arr, int start, int end)
+  private static String trimmedStr(String str, int start, int end)
   {
-    while (arr[start] <= 0x20)
+    while (str.charAt(start) <= 0x20)
     {
       start++;
     }
-    while (arr[end - 1] <= 0x20)
+    while (str.charAt(end - 1) <= 0x20)
     {
       end--;
     }
-    return start >= end ? "" : new String(arr, start, end - start);
+    return start >= end ? "" : str.substring(start, end);
   }
 }
