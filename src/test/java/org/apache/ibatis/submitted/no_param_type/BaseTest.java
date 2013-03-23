79096cfbabe442c06c65c8beea614912fc020a9a
Iwao AVE! <harawata@gmail.com>
2013-03-23 23:35:39 +0900

    Added a test case for a corner case.

---
+++ src/test/java/org/apache/ibatis/submitted/no_param_type/BaseTest.java
@@ -0,0 +1,122 @@
+/*
+ *    Copyright 2009-2012 The MyBatis Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.submitted.no_param_type;
+
+import static org.junit.Assert.*;
+
+import java.io.Reader;
+import java.sql.Connection;
+import java.util.List;
+
+import org.apache.ibatis.executor.BatchResult;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BaseTest {
+
+  private static SqlSessionFactory sqlSessionFactory;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    // create a SqlSessionFactory
+    Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/no_param_type/mybatis-config.xml");
+    sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    reader.close();
+
+    // populate in-memory database
+    SqlSession session = sqlSessionFactory.openSession();
+    Connection conn = session.getConnection();
+    reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/no_param_type/CreateDB.sql");
+    ScriptRunner runner = new ScriptRunner(conn);
+    runner.setLogWriter(null);
+    runner.runScript(reader);
+    reader.close();
+    session.close();
+  }
+
+  @Test
+  public void shouldAcceptDifferentTypeInTheSameBatch() {
+    SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
+    try {
+      ObjA a = new ObjA();
+      a.setId(1);
+      a.setName(111);
+      sqlSession.insert("insertUser", a);
+      ObjB b = new ObjB();
+      b.setId(2);
+      b.setName("222");
+      sqlSession.insert("insertUser", b);
+      List<BatchResult> batchResults = sqlSession.flushStatements();
+      batchResults.clear();
+      sqlSession.clearCache();
+      sqlSession.commit();
+      List<User> users = sqlSession.selectList("selectUser");
+      assertEquals(2, users.size());
+    } finally {
+      sqlSession.close();
+    }
+  }
+
+  public static class ObjA {
+    private Integer id;
+
+    private Integer name;
+
+    public Integer getId() {
+      return id;
+    }
+
+    public void setId(Integer id) {
+      this.id = id;
+    }
+
+    public Integer getName() {
+      return name;
+    }
+
+    public void setName(Integer name) {
+      this.name = name;
+    }
+  }
+
+  public static class ObjB {
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+      return id;
+    }
+
+    public void setId(Integer id) {
+      this.id = id;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
+}
