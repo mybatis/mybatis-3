b093f78a525caeedd3645a34f38a1c5d56adb8f4
Eduardo Macarron <eduardo.macarron@gmail.com>
2013-04-10 19:33:12 +0200

    Related to #28. Log4j 2 wrapper impl.

---
+++ src/main/java/org/apache/ibatis/logging/log4j2/Log4j2Impl.java
@@ -0,0 +1,64 @@
+/*
+ *    Copyright 2009-2012 The MyBatis Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.logging.log4j2;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
+public class Log4j2Impl implements Log {
+  
+  private static Marker MARKER = MarkerManager.getMarker(LogFactory.MARKER);
+
+  private Logger log;
+
+  public Log4j2Impl(String clazz) {
+    // how is the FQCN provided in log4j2???
+    log = LogManager.getLogger(clazz);
+  }
+
+  public boolean isDebugEnabled() {
+    return log.isDebugEnabled();
+  }
+
+  public boolean isTraceEnabled() {
+    return log.isTraceEnabled();
+  }
+
+  public void error(String s, Throwable e) {
+    log.error(MARKER, s, e);
+  }
+
+  public void error(String s) {
+    log.error(MARKER, s);
+  }
+
+  public void debug(String s) {
+    log.debug(MARKER, s);
+  }
+
+  public void trace(String s) {
+    log.trace(MARKER, s);
+  }
+
+  public void warn(String s) {
+    log.warn(MARKER, s);
+  }
+
+}
