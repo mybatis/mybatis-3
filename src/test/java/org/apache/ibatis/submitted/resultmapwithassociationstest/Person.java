9d07e69aacc944ce790144f0d972a6e42bfc16cc
Eduardo Macarron <eduardo.macarron@gmail.com>
2013-11-19 21:01:41 +0100

    Re-used test for issue #99.

--- src/test/java/org/apache/ibatis/submitted/resultmapwithassociationstest/Person.java
+++ src/test/java/org/apache/ibatis/submitted/resultmapwithassociationstest/Person.java
@@ -15,23 +15,25 @@
  */
 package org.apache.ibatis.submitted.resultmapwithassociationstest;
 
+import java.util.List;
+
 public class Person {
-    private int id;
-    private Address address;
+  private int id;
+  private List<Address> addresses;
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
+  public void setId(final int id) {
+    this.id = id;
+  }
 
-    public Address getAddress() {
-        return address;
-    }
+  public List<Address> getAddresses() {
+    return addresses;
+  }
 
-    public void setAddress(final Address address) {
-        this.address = address;
-    }
+  public void setAddresses(final List<Address> addresses) {
+    this.addresses = addresses;
+  }
 }
--- src/test/java/org/apache/ibatis/submitted/resultmapwithassociationstest/ResultMapWithAssociationsTest.java
+++ src/test/java/org/apache/ibatis/submitted/resultmapwithassociationstest/ResultMapWithAssociationsTest.java
@@ -19,47 +19,47 @@ import java.io.Reader;
 import java.sql.Connection;
 import java.util.List;
 
-import org.apache.ibatis.exceptions.PersistenceException;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ResultMapWithAssociationsTest {
 
-    private static SqlSessionFactory sqlSessionFactory;
+  private static SqlSessionFactory sqlSessionFactory;
 
-    @Test(expected=PersistenceException.class)
-    public void testMissingColumn() throws Exception {
-        // create a SqlSessionFactory
-        Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/resultmapwithassociationstest/mybatis-config.xml");
-        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        reader.close();
+  @BeforeClass
+  public static void setUp() throws Exception {
+    // create a SqlSessionFactory
+    Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/resultmapwithassociationstest/mybatis-config.xml");
+    sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    reader.close();
 
-        // populate in-memory database
-        SqlSession session = sqlSessionFactory.openSession();
-        Connection conn = session.getConnection();
-        reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/resultmapwithassociationstest/CreateDB.sql");
-        ScriptRunner runner = new ScriptRunner(conn);
-        runner.setLogWriter(null);
-        runner.runScript(reader);
-        reader.close();
-        session.close();
+    // populate in-memory database
+    SqlSession session = sqlSessionFactory.openSession();
+    Connection conn = session.getConnection();
+    reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/resultmapwithassociationstest/CreateDB.sql");
+    ScriptRunner runner = new ScriptRunner(conn);
+    runner.setLogWriter(null);
+    runner.runScript(reader);
+    reader.close();
+    session.close();
+  }
+
+  @Test
+  public void shouldFindAllPersonRecordsWithAssociatedAddressRecord() {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      Mapper mapper = sqlSession.getMapper(Mapper.class);
+      List<Person> resultList = mapper.findAll();
+      Assert.assertEquals(3, resultList.size());
+    } finally {
+      sqlSession.close();
     }
-
-    public void shouldFindAllPersonRecordsWithAssociatedAddressRecord() {
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        try {
-            Mapper mapper = sqlSession.getMapper(Mapper.class);
-            List<Person> resultList = mapper.findAll();
-            Assert.assertEquals(3, resultList.size());
-        }
-        finally {
-            sqlSession.close();
-        }
-    }
+  }
 
 }
