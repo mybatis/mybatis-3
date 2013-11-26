cf323fd51f191e167a2cfa61965908209688e3d7
miaogr86 <miaogr86@gmail.com>
2013-11-27 00:34:56 +0800

    Fix

--- src/main/java/org/apache/ibatis/cache/decorators/WeakCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/WeakCache.java
@@ -23,87 +23,93 @@ import java.util.concurrent.locks.ReadWriteLock;
 import org.apache.ibatis.cache.Cache;
 
 /**
- * Weak Reference cache decorator.
- * Thanks to Dr. Heinz Kabutz for his guidance here.
+ * Weak Reference cache decorator. Thanks to Dr. Heinz Kabutz for his guidance
+ * here.
  */
 public class WeakCache implements Cache {
-  private final LinkedList<Object> hardLinksToAvoidGarbageCollection;
-  private final ReferenceQueue<Object> queueOfGarbageCollectedEntries;
-  private final Cache delegate;
-  private int numberOfHardLinks;
+	private final LinkedList<Object> hardLinksToAvoidGarbageCollection;
+	private final ReferenceQueue<Object> queueOfGarbageCollectedEntries;
+	private final Cache delegate;
+	private int numberOfHardLinks;
 
-  public WeakCache(Cache delegate) {
-    this.delegate = delegate;
-    this.numberOfHardLinks = 256;
-    this.hardLinksToAvoidGarbageCollection = new LinkedList<Object>();
-    this.queueOfGarbageCollectedEntries = new ReferenceQueue<Object>();
-  }
+	public WeakCache(Cache delegate) {
+		this.delegate = delegate;
+		this.numberOfHardLinks = 256;
+		this.hardLinksToAvoidGarbageCollection = new LinkedList<Object>();
+		this.queueOfGarbageCollectedEntries = new ReferenceQueue<Object>();
+	}
 
-  public String getId() {
-    return delegate.getId();
-  }
+	public String getId() {
+		return delegate.getId();
+	}
 
-  public int getSize() {
-    removeGarbageCollectedItems();
-    return delegate.getSize();
-  }
+	public int getSize() {
+		removeGarbageCollectedItems();
+		return delegate.getSize();
+	}
 
-  public void setSize(int size) {
-    this.numberOfHardLinks = size;
-  }
+	public void setSize(int size) {
+		this.numberOfHardLinks = size;
+	}
 
-  public void putObject(Object key, Object value) {
-    removeGarbageCollectedItems();
-    delegate.putObject(key, new WeakEntry(key, value, queueOfGarbageCollectedEntries));
-  }
+	public void putObject(Object key, Object value) {
+		removeGarbageCollectedItems();
+		delegate.putObject(key, new WeakEntry(key, value,
+				queueOfGarbageCollectedEntries));
+	}
 
-  public Object getObject(Object key) {
-    Object result = null;
-    @SuppressWarnings("unchecked") // assumed delegate cache is totally managed by this cache
-    WeakReference<Object> weakReference = (WeakReference<Object>) delegate.getObject(key);
-    if (weakReference != null) {
-      result = weakReference.get();
-      if (result == null) {
-        delegate.removeObject(key);
-      } else {
-        hardLinksToAvoidGarbageCollection.addFirst(result);
-        if (hardLinksToAvoidGarbageCollection.size() > numberOfHardLinks) {
-          hardLinksToAvoidGarbageCollection.removeLast();
-        }
-      }
-    }
-    return result;
-  }
+	public Object getObject(Object key) {
+		Object result = null;
+		@SuppressWarnings("unchecked")
+		// assumed delegate cache is totally managed by this cache
+		WeakReference<Object> weakReference = (WeakReference<Object>) delegate
+				.getObject(key);
+		if (weakReference != null) {
+			result = weakReference.get();
+			if (result == null) {
+				delegate.removeObject(key);
+			} else {
+				synchronized (hardLinksToAvoidGarbageCollection) {
+					hardLinksToAvoidGarbageCollection.addFirst(result);
+					if (hardLinksToAvoidGarbageCollection.size() > numberOfHardLinks) {
+						hardLinksToAvoidGarbageCollection.removeLast();
+					}
+				}
+			}
+		}
+		return result;
+	}
 
-  public Object removeObject(Object key) {
-    removeGarbageCollectedItems();
-    return delegate.removeObject(key);
-  }
+	public Object removeObject(Object key) {
+		removeGarbageCollectedItems();
+		return delegate.removeObject(key);
+	}
 
-  public void clear() {
-    hardLinksToAvoidGarbageCollection.clear();
-    removeGarbageCollectedItems();
-    delegate.clear();
-  }
+	public void clear() {
+		hardLinksToAvoidGarbageCollection.clear();
+		removeGarbageCollectedItems();
+		delegate.clear();
+	}
 
-  public ReadWriteLock getReadWriteLock() {
-    return delegate.getReadWriteLock();
-  }
+	public ReadWriteLock getReadWriteLock() {
+		return delegate.getReadWriteLock();
+	}
 
-  private void removeGarbageCollectedItems() {
-    WeakEntry sv;
-    while ((sv = (WeakEntry) queueOfGarbageCollectedEntries.poll()) != null) {
-      delegate.removeObject(sv.key);
-    }
-  }
+	private void removeGarbageCollectedItems() {
+		WeakEntry sv;
+		while ((sv = (WeakEntry) queueOfGarbageCollectedEntries.poll()) != null) {
+			delegate.removeObject(sv.key);
+		}
+	}
 
-  private static class WeakEntry extends WeakReference<Object> {
-    private final Object key;
+	private static class WeakEntry extends WeakReference<Object> {
+		private final Object key;
 
-    private WeakEntry(Object key, Object value, ReferenceQueue<Object> garbageCollectionQueue) {
-      super(value, garbageCollectionQueue);
-      this.key = key;
-    }
-  }
+		private WeakEntry(Object key, Object value,
+				ReferenceQueue<Object> garbageCollectionQueue) {
+			super(value, garbageCollectionQueue);
+			this.key = key;
+		}
+	}
 
 }
