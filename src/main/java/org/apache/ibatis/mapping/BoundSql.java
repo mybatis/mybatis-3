7552bb7aa10d96c388fed654dd118317a7bb8948
Eduardo Macarron <eduardo.macarron@gmail.com>
2014-01-03 10:52:48 +0100

    Some javadocs [ci skip]

--- src/main/java/org/apache/ibatis/mapping/BoundSql.java
+++ src/main/java/org/apache/ibatis/mapping/BoundSql.java
@@ -22,6 +22,14 @@ import java.util.Map;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.session.Configuration;
 
+/**
+ * An actual SQL String got form an {@link SqlSource} after having processed any dynamic content.
+ * The SQL may have SQL placeholders "?" and an list (ordered) of an parameter mappings 
+ * with the additional information for each parameter (at least the property name of the input object to read 
+ * the value from). 
+ * </br>
+ * Can also have additional parameters that are created by the dynamic language (for loops, bind...).
+ */
 public class BoundSql {
 
   private String sql;
--- src/main/java/org/apache/ibatis/scripting/LanguageDriver.java
+++ src/main/java/org/apache/ibatis/scripting/LanguageDriver.java
@@ -20,13 +20,15 @@ import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
 import org.apache.ibatis.parsing.XNode;
+import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
 import org.apache.ibatis.session.Configuration;
 
 public interface LanguageDriver {
 
   /**
-   * Creates a {@link ParameterHandler} that will set the parameters of the
-   *  
+   * Creates a {@link ParameterHandler} that passes the actual parameters to the the JDBC statement.
+   * 
+   * @see DefaultParameterHandler
    * @param mappedStatement The mapped statement that is being executed
    * @param parameterObject The input parameter object (can be null) 
    * @param boundSql The resulting SQL once the dynamic language has been executed.
@@ -35,7 +37,8 @@ public interface LanguageDriver {
   ParameterHandler createParameterHandler(MappedStatement mappedStatement, Object parameterObject, BoundSql boundSql);
 
   /**
-   * Creates an {@link SqlSource} that will hold the statement read from a mapper xml file
+   * Creates an {@link SqlSource} that will hold the statement read from a mapper xml file. 
+   * It is called during startup, when the mapped statement is read from a class or an xml file.
    * 
    * @param configuration The MyBatis configuration
    * @param script XNode parsed from a XML file
@@ -45,7 +48,8 @@ public interface LanguageDriver {
   SqlSource createSqlSource(Configuration configuration, XNode script, Class<?> parameterType);
 
   /**
-   * Creates an {@link SqlSource} that will hold the statement read from an annotation
+   * Creates an {@link SqlSource} that will hold the statement read from an annotation.
+   * It is called during startup, when the mapped statement is read from a class or an xml file.
    * 
    * @param configuration The MyBatis configuration
    * @param script The content of the annotation
