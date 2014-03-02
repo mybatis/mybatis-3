25da37cfdc25155477a53270b92debb033b9f0c3
Eduardo Macarron <eduardo.macarron@gmail.com>
2014-03-02 09:43:27 +0100

    No need to create an in-memory db

--- src/test/java/org/apache/ibatis/submitted/integer_enum/IntegerEnumTest.java
+++ src/test/java/org/apache/ibatis/submitted/integer_enum/IntegerEnumTest.java
@@ -15,35 +15,21 @@
  */
 package org.apache.ibatis.submitted.integer_enum;
 
+import java.io.Reader;
+
 import org.apache.ibatis.BaseDataTest;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-
-import javax.sql.DataSource;
-import java.io.Reader;
-import java.sql.Connection;
 
 public class IntegerEnumTest extends BaseDataTest {
 
   @Test
   public void shouldParseMapWithIntegerJdbcType() throws Exception {
-    DataSource ds = BaseDataTest.createJPetstoreDataSource();
-    Connection conn = ds.getConnection();
     String resource = "org/apache/ibatis/submitted/integer_enum/MapperConfig.xml";
     Reader reader = Resources.getResourceAsReader(resource);
     SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-    SqlSessionFactory sqlMapper = builder.build(reader);
-    SqlSession session = sqlMapper.openSession(conn);
-    try {
-      assertNotNull(session);
-    } finally {
-      if (session != null) session.close();
-      if (conn != null) conn.close();
-    }
+    builder.build(reader);
   }
 
 }
