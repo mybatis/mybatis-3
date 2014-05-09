9476328e518daca9e61c25d5d92f8f76813dc4bd
Iwao AVE! <harawata@gmail.com>
2014-05-09 21:05:01 +0900

    fixes #194 Incorrect parse result when a variable precedes a skipped variable.

--- src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
+++ src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
@@ -39,7 +39,7 @@ public class GenericTokenParser {
       while (start > -1) {
         if (start > 0 && src[start - 1] == '\\') {
           // the variable is escaped. remove the backslash.
-          builder.append(src, offset, start - 1).append(openToken);
+          builder.append(src, offset, start - offset - 1).append(openToken);
           offset = start + openToken.length();
         } else {
           int end = text.indexOf(closeToken, start);
