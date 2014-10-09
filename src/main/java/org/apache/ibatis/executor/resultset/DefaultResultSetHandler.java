e035b1ce2d2e01978949760d609f46b70366cedb
Eduardo Macarron <eduardo.macarron@gmail.com>
2014-10-09 21:56:20 +0200

    Merging minor fix for 3.2.x

--- src/main/java/org/apache/ibatis/executor/resultset/DefaultResultSetHandler.java
+++ src/main/java/org/apache/ibatis/executor/resultset/DefaultResultSetHandler.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.ErrorContext;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.ExecutorException;
 import org.apache.ibatis.executor.loader.ResultLoader;
@@ -137,6 +138,8 @@ public class DefaultResultSetHandler implements ResultSetHandler {
   //
 
   public List<Object> handleResultSets(Statement stmt) throws SQLException {
+    ErrorContext.instance().activity("handling results").object(mappedStatement.getId());
+    
     final List<Object> multipleResults = new ArrayList<Object>();
 
     int resultSetCount = 0;
