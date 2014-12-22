66efde54cb8c68088c2b3bd24024559fcdf67697
Eduardo Macarron <eduardo.macarron@gmail.com>
2014-12-22 20:09:31 +0100

    ASL headers and formatting [ci skip]. Related to #307.

--- src/test/java/org/apache/ibatis/submitted/nestedresulthandler_multiple_association/NestedResultHandlerMultipleAssociationTest.java
+++ src/test/java/org/apache/ibatis/submitted/nestedresulthandler_multiple_association/NestedResultHandlerMultipleAssociationTest.java
@@ -30,60 +30,63 @@ import org.junit.Test;
 
 public class NestedResultHandlerMultipleAssociationTest {
 
-	private static SqlSessionFactory sqlSessionFactory;
+  private static SqlSessionFactory sqlSessionFactory;
 
-	@BeforeClass
-	public static void setUp() throws Exception {
-		// create an SqlSessionFactory
-		Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/nestedresulthandler_multiple_association/mybatis-config.xml");
-		sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-		reader.close();
+  @BeforeClass
+  public static void setUp() throws Exception {
+    // create an SqlSessionFactory
+    Reader reader = Resources
+        .getResourceAsReader("org/apache/ibatis/submitted/nestedresulthandler_multiple_association/mybatis-config.xml");
+    sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    reader.close();
 
-		// populate in-memory database
-		SqlSession session = sqlSessionFactory.openSession();
-		Connection conn = session.getConnection();
-		reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/nestedresulthandler_multiple_association/CreateDB.sql");
-		ScriptRunner runner = new ScriptRunner(conn);
-		runner.setLogWriter(null);
-		runner.runScript(reader);
-		reader.close();
-		session.close();
-	}
+    // populate in-memory database
+    SqlSession session = sqlSessionFactory.openSession();
+    Connection conn = session.getConnection();
+    reader = Resources
+        .getResourceAsReader("org/apache/ibatis/submitted/nestedresulthandler_multiple_association/CreateDB.sql");
+    ScriptRunner runner = new ScriptRunner(conn);
+    runner.setLogWriter(null);
+    runner.runScript(reader);
+    reader.close();
+    session.close();
+  }
 
-	@Test
-	public void failure() throws Exception {
-		SqlSession sqlSession = sqlSessionFactory.openSession();
+  @Test
+  public void failure() throws Exception {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
 
-		// Parents have child going from somewhere to somewhere, they are stored in a Binome object
-		// In this test we have 2 parents:
-		// Parent1 is going from Child1 to Child2
-		// Parent2 is going from Child2 to Child3 and from Child1 to Child2
-		// You'll see a NULL entry in the list instead of the Binome Child1/Child2
-		List<ParentBean> list = sqlSession.selectList("selectParentBeans");
-		for (ParentBean pb : list) {
-			for (Binome<ChildBean, ChildBean> childs : pb.getChilds()) {
-				Assert.assertNotNull(childs);
-				Assert.assertNotNull(childs.getOne());
-				Assert.assertNotNull(childs.getTwo());
-			}
-		}
+    // Parents have child going from somewhere to somewhere, they are stored in
+    // a Binome object
+    // In this test we have 2 parents:
+    // Parent1 is going from Child1 to Child2
+    // Parent2 is going from Child2 to Child3 and from Child1 to Child2
+    // You'll see a NULL entry in the list instead of the Binome Child1/Child2
+    List<ParentBean> list = sqlSession.selectList("selectParentBeans");
+    for (ParentBean pb : list) {
+      for (Binome<ChildBean, ChildBean> childs : pb.getChilds()) {
+        Assert.assertNotNull(childs);
+        Assert.assertNotNull(childs.getOne());
+        Assert.assertNotNull(childs.getTwo());
+      }
+    }
 
-		sqlSession.close();
-	}
+    sqlSession.close();
+  }
 
-	@Test
-	public void success() throws Exception {
-		SqlSession sqlSession = sqlSessionFactory.openSession();
+  @Test
+  public void success() throws Exception {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
 
-		ParentBean parent = sqlSession.selectOne("selectParentBeanById", 2);
+    ParentBean parent = sqlSession.selectOne("selectParentBeanById", 2);
 
-		// If you only select the Parent2 it works
-		for (Binome<ChildBean, ChildBean> childs : parent.getChilds()) {
-			Assert.assertNotNull(childs);
-			Assert.assertNotNull(childs.getOne());
-			Assert.assertNotNull(childs.getTwo());
-		}
-		sqlSession.close();
-	}
+    // If you only select the Parent2 it works
+    for (Binome<ChildBean, ChildBean> childs : parent.getChilds()) {
+      Assert.assertNotNull(childs);
+      Assert.assertNotNull(childs.getOne());
+      Assert.assertNotNull(childs.getTwo());
+    }
+    sqlSession.close();
+  }
 
 }
--- src/test/java/org/apache/ibatis/submitted/nestedresulthandler_multiple_association/ParentBean.java
+++ src/test/java/org/apache/ibatis/submitted/nestedresulthandler_multiple_association/ParentBean.java
@@ -3,40 +3,41 @@ package org.apache.ibatis.submitted.nestedresulthandler_multiple_association;
 import java.util.List;
 
 public class ParentBean {
-	private Integer id;
-	private String value;
-	private List<Binome<ChildBean, ChildBean>> childs;
+  private Integer id;
+  private String value;
+  private List<Binome<ChildBean, ChildBean>> childs;
 
-	public Integer getId() {
-		return id;
-	}
+  public Integer getId() {
+    return id;
+  }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public String getValue() {
-		return value;
-	}
+  public String getValue() {
+    return value;
+  }
 
-	public void setValue(String value) {
-		this.value = value;
-	}
+  public void setValue(String value) {
+    this.value = value;
+  }
 
-	public List<Binome<ChildBean, ChildBean>> getChilds() {
-		return childs;
-	}
+  public List<Binome<ChildBean, ChildBean>> getChilds() {
+    return childs;
+  }
 
-	public void setChilds(List<Binome<ChildBean, ChildBean>> childs) {
-		this.childs = childs;
-	}
+  public void setChilds(List<Binome<ChildBean, ChildBean>> childs) {
+    this.childs = childs;
+  }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder("ParentBean [id=" + id + ", value=" + value + "]\nChilds:\n");
-		for (Binome<ChildBean, ChildBean> binome : childs) {
-			sb.append("\tChild : ").append(binome).append('\n');
-		}
-		return sb.toString();
-	}
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("ParentBean [id=" + id + ", value="
+        + value + "]\nChilds:\n");
+    for (Binome<ChildBean, ChildBean> binome : childs) {
+      sb.append("\tChild : ").append(binome).append('\n');
+    }
+    return sb.toString();
+  }
 }
