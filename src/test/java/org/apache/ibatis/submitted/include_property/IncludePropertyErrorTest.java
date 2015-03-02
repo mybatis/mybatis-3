e99abe6659f285d6fa8207480fe4eda02aa5fb1e
Iwao AVE! <harawata@gmail.com>
2015-03-02 15:40:50 +0900

    #331 Added tests for spec coverage.

---
+++ src/test/java/org/apache/ibatis/submitted/include_property/IncludePropertyErrorTest.java
@@ -0,0 +1,30 @@
+/**
+ *    Copyright 2009-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.submitted.include_property;
+
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.ibatis.session.Configuration;
+import org.junit.Test;
+
+public class IncludePropertyErrorTest {
+
+  @Test(expected = PersistenceException.class)
+  public void shouldFailForDuplicatedPropertyNames() throws Exception {
+    Configuration configuration = new Configuration();
+    configuration.addMapper(DuplicatedIncludePropertiesMapper.class);
+  }
+
+}
