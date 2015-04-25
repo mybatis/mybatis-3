779679b9011300331bdb21f0af81844d7ff5a933
Eduardo Macarron <eduardo.macarron@gmail.com>
2015-04-25 17:45:05 +0200

    No need to check the class type.

--- src/main/java/org/apache/ibatis/scripting/LanguageDriverRegistry.java
+++ src/main/java/org/apache/ibatis/scripting/LanguageDriverRegistry.java
@@ -31,9 +31,6 @@ public class LanguageDriverRegistry {
     if (cls == null) {
       throw new IllegalArgumentException("null is not a valid Language Driver");
     }
-    if (!LanguageDriver.class.isAssignableFrom(cls)) {
-      throw new ScriptingException(cls.getName() + " does not implement " + LanguageDriver.class.getName());
-    }
     LanguageDriver driver = LANGUAGE_DRIVER_MAP.get(cls);
     if (driver == null) {
       try {
@@ -48,9 +45,6 @@ public class LanguageDriverRegistry {
   public void register(LanguageDriver instance) {
     if (instance == null) {
       throw new IllegalArgumentException("null is not a valid Language Driver");
-    }
-    if (!LanguageDriver.class.isAssignableFrom(instance.getClass())) {
-      throw new ScriptingException(instance.getClass().getName() + " does not implement " + LanguageDriver.class.getName());
     }
     LanguageDriver driver = LANGUAGE_DRIVER_MAP.get(instance.getClass());
     if (driver == null) {
