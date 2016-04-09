2c61e6e1aeafd46af4c18039a995206ead8d6377
Eduardo Macarron <eduardo.macarron@gmail.com>
2016-04-09 19:21:04 +0200

    fixing broken build

--- src/main/java/org/apache/ibatis/annotations/Arg.java
+++ src/main/java/org/apache/ibatis/annotations/Arg.java
@@ -40,7 +40,7 @@ public @interface Arg {
 
   JdbcType jdbcType() default JdbcType.UNDEFINED;
 
-  Class<? extends TypeHandler<?>> typeHandler() default UnknownTypeHandler.class;
+  Class<? extends TypeHandler> typeHandler() default UnknownTypeHandler.class;
 
   String select() default "";
 
