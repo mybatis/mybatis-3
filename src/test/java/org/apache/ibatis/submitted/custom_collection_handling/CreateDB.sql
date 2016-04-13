543d464d93efa273eb5f2c76f049420f0d33716c
Jeremy Landis <jeremylandis@hotmail.com>
2016-04-12 21:57:50 -0400

    Update parent to 27-snapshot and let it update licenses (#632)
    * [license] Add inception year, update to parent 27-SNAPSHOT, and add git setting
    
    * [license] Updated all licenses to ensure we are updating all of them

--- src/test/java/org/apache/ibatis/submitted/custom_collection_handling/CreateDB.sql
+++ src/test/java/org/apache/ibatis/submitted/custom_collection_handling/CreateDB.sql
@@ -1,5 +1,5 @@
 --
---    Copyright 2009-2012 the original author or authors.
+--    Copyright 2009-2016 the original author or authors.
 --
 --    Licensed under the Apache License, Version 2.0 (the "License");
 --    you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 --    See the License for the specific language governing permissions and
 --    limitations under the License.
 --
+
 drop table if exists contact;
 drop table if exists person;
 create table person (
