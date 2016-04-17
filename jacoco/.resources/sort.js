bf72fc993f4bf6d68ebf37416dcb6a4ea16b19e5
Jeremy Landis <jeremylandis@hotmail.com>
2016-04-17 14:04:05 -0400

    wagon-git commit

--- jacoco/.resources/sort.js
+++ jacoco/.resources/sort.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 Mountainminds GmbH & Co. KG and Contributors
+ * Copyright (c) 2009, 2016 Mountainminds GmbH & Co. KG and Contributors
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
