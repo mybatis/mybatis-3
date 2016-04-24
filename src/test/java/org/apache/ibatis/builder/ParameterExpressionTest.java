d026ad25635bc1098742b2f397446e9cbf41dc57
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-04-25 03:30:38 +0900

    Add test for ParameterExpression

--- src/test/java/org/apache/ibatis/builder/ParameterExpressionTest.java
+++ src/test/java/org/apache/ibatis/builder/ParameterExpressionTest.java
@@ -17,9 +17,16 @@ package org.apache.ibatis.builder;
 
 import java.util.Map;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.core.Is.is;
 
 public class ParameterExpressionTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
 
   @Test
   public void simpleProperty() {
@@ -122,6 +129,13 @@ public class ParameterExpressionTest {
     Assert.assertEquals("VARCHAR", result.get("jdbcType"));
     Assert.assertEquals("val1", result.get("attr1"));
     Assert.assertEquals("val2", result.get("attr2"));
+  }
+
+  @Test
+  public void invalidOldJdbcTypeFormat() {
+    expectedException.expect(BuilderException.class);
+    expectedException.expectMessage(is("Parsing error in {id:} in position 3"));
+    new ParameterExpression("id:");
   }
 
 }
