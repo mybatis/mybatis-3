2debce63b5c4b00c9a5df15bab210d08011bc3e3
Iwao AVE! <harawata@gmail.com>
2016-07-14 08:31:11 +0900

    SonarQube was not updated even though the build succeeded. Reverting back to original method.

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -50,13 +50,13 @@ if [ "$mybatis_repo" == "https://github.com/mybatis/mybatis-3.git" ] && [ "$TRAV
   if [ $VER == "18" ]; then
     mvn clean deploy -Dmaven.test.skip=true -q --settings ./travis/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-    # mvn clean test jacoco:report coveralls:report -q
-    # echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
+    mvn clean test jacoco:report coveralls:report -q
+    echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
     # various issues exist currently in building this so comment for now
     # mvn site site:deploy -q
     # echo -e "Successfully deploy site under Travis job ${TRAVIS_JOB_NUMBER}"
-    mvn clean jacoco:prepare-agent package sonar:sonar jacoco:report coveralls:report -Dsonar.host.url=https://sonarqube.com -Dsonar.login=ccf0be39fd0ca5ea5aa712247c79da7233cd3caa -q
-    echo -e "Successfully ran coveralls and Sonar integration under Travis job ${TRAVIS_JOB_NUMBER}"
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=ccf0be39fd0ca5ea5aa712247c79da7233cd3caa
+    echo -e "Successfully ran Sonar integration under Travis job ${TRAVIS_JOB_NUMBER}"
   fi
 else
   echo "Travis build skipped"
