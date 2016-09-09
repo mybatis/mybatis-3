ab6c54c1fc4d6e40e2143341be4bc715fd5e3502
yangzheng <yangzheng@xish.com>
2016-09-09 13:25:00 +0800

    Change the DefaultSqlSession.selectOne, because the result value of selectList can be null.

--- src/main/java/org/apache/ibatis/session/defaults/DefaultSqlSession.java
+++ src/main/java/org/apache/ibatis/session/defaults/DefaultSqlSession.java
@@ -75,9 +75,9 @@ public class DefaultSqlSession implements SqlSession {
   public <T> T selectOne(String statement, Object parameter) {
     // Popular vote was to return null on 0 results and throw exception on too many.
     List<T> list = this.<T>selectList(statement, parameter);
-    if (list.size() == 1) {
+    if (list != null && list.size() == 1) {
       return list.get(0);
-    } else if (list.size() > 1) {
+    } else if (list != null && list.size() > 1) {
       throw new TooManyResultsException("Expected one result (or null) to be returned by selectOne(), but found: " + list.size());
     } else {
       return null;
