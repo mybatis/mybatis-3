000fd4c6e5d7f6969fde789caecf317398f2a7ef
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-11-20 23:46:34 +0900

    Remove unnecessary target from some annotations

--- src/main/java/org/apache/ibatis/annotations/Case.java
+++ src/main/java/org/apache/ibatis/annotations/Case.java
@@ -16,7 +16,6 @@
 package org.apache.ibatis.annotations;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -26,7 +25,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({})
 public @interface Case {
   String value();
 
--- src/main/java/org/apache/ibatis/plugin/Signature.java
+++ src/main/java/org/apache/ibatis/plugin/Signature.java
@@ -26,7 +26,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Target({})
 public @interface Signature {
   Class<?> type();
 
