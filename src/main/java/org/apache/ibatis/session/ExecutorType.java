b1e3c49f717ee14c861eed6ef36fe46b1eb30103
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-12-11 23:10:11 +0900

    Add the MergingBatchExecutor This executor is the Batch Executor for merging an sql that created using same mapper method regardless of calling order.

--- src/main/java/org/apache/ibatis/session/ExecutorType.java
+++ src/main/java/org/apache/ibatis/session/ExecutorType.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
+ *    Copyright 2009-2016 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,9 +15,37 @@
  */
 package org.apache.ibatis.session;
 
+import org.apache.ibatis.executor.MergingBatchExecutor;
+
 /**
+ * Executor type.
+ *
+ * @see org.apache.ibatis.executor.Executor
  * @author Clinton Begin
+ * @author Kazuki Shimizu
+ *
  */
 public enum ExecutorType {
-  SIMPLE, REUSE, BATCH
+  /**
+   * Enable the simple executor.
+   * @see org.apache.ibatis.executor.SimpleExecutor
+   *
+   */
+  SIMPLE,
+  /**
+   * Enable the executor for reusing a prepared statement.
+   * @see org.apache.ibatis.executor.ReuseExecutor
+   */
+  REUSE,
+  /**
+   * Enable the batch executor. (valid only when consecutively calling the same mapper method)
+   * @see org.apache.ibatis.executor.BatchExecutor
+   */
+  BATCH,
+  /**
+   * Enable the batch executor for merging an SQL that created using same mapper method regardless of calling order.
+   * @see MergingBatchExecutor
+   * @since 3.4.2
+   */
+  MERGING_BATCH
 }
