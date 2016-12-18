3e574cad2828f3a70defca7b5b76ebaf72535e9b
Jeremy Landis <jeremylandis@hotmail.com>
2016-12-17 23:58:44 -0500

    [test] Fix memory leak

--- src/test/java/org/apache/ibatis/io/ExternalResourcesTest.java
+++ src/test/java/org/apache/ibatis/io/ExternalResourcesTest.java
@@ -86,6 +86,7 @@ public class ExternalResourcesTest {
       FileWriter fileWriter = new FileWriter(tempFile);
       fileWriter.append("new_command.template=templates/col_new_template_migration.sql");
       fileWriter.flush();
+      fileWriter.close();
       templateName = ExternalResources.getConfiguredTemplate(tempFile.getAbsolutePath(), "new_command.template");
       assertEquals("templates/col_new_template_migration.sql", templateName);
     } catch (Exception e) {
