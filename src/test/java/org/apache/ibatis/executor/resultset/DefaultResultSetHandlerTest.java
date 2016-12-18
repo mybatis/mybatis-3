44035d7ef43fd4b513619de30a8240dc0c9949f2
Jeremy Landis <jeremylandis@hotmail.com>
2016-12-17 23:58:20 -0500

    squash unused import

--- src/test/java/org/apache/ibatis/executor/resultset/DefaultResultSetHandlerTest.java
+++ src/test/java/org/apache/ibatis/executor/resultset/DefaultResultSetHandlerTest.java
@@ -17,7 +17,6 @@ package org.apache.ibatis.executor.resultset;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
