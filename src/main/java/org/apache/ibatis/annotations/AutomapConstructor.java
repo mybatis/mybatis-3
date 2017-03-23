75b6b13b24d53ef20a3c740d409b98a8d8ae8832
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-03-24 01:09:22 +0900

    Add javadoc at AutomapConstructor See gh-809

--- src/main/java/org/apache/ibatis/annotations/AutomapConstructor.java
+++ src/main/java/org/apache/ibatis/annotations/AutomapConstructor.java
@@ -15,8 +15,18 @@
  */
 package org.apache.ibatis.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * The marker annotation that indicate a constructor for automatic mapping.
+ *
+ * @author Tim Chen
+ * @since 3.4.3
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.CONSTRUCTOR})
