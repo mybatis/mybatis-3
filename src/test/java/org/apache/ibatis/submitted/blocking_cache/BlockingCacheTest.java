070b14ecaeb727a9da85fd1509a334c9e1f74b7b
Jeremy Landis <jeremylandis@hotmail.com>
2017-04-18 21:57:50 -0400

    [ci] Use assert fail instead of printStackTrace

--- src/test/java/org/apache/ibatis/submitted/blocking_cache/BlockingCacheTest.java
+++ src/test/java/org/apache/ibatis/submitted/blocking_cache/BlockingCacheTest.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
+ *    Copyright 2009-2017 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -85,7 +85,7 @@ public class BlockingCacheTest {
       try {
         Thread.sleep(500);
       } catch (InterruptedException e) {
-        e.printStackTrace();
+        Assert.fail(e.getMessage());
       }
     } finally {
       sqlSession1.close();
