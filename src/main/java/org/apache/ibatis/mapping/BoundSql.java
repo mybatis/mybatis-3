d09b6f63305c82fd1ec80de0e638ebec765eccdf
cyflhn <cyflhn@163.com>
2017-08-02 17:30:11 +0800

    add cache for in-parameter to avoid getting in-parameter repeatedly

--- src/main/java/org/apache/ibatis/mapping/BoundSql.java
+++ src/main/java/org/apache/ibatis/mapping/BoundSql.java
@@ -1,21 +1,16 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 package org.apache.ibatis.mapping;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -24,53 +19,67 @@ import org.apache.ibatis.reflection.property.PropertyTokenizer;
 import org.apache.ibatis.session.Configuration;
 
 /**
- * An actual SQL String got from an {@link SqlSource} after having processed any dynamic content.
- * The SQL may have SQL placeholders "?" and an list (ordered) of an parameter mappings 
- * with the additional information for each parameter (at least the property name of the input object to read 
- * the value from). 
- * </br>
+ * An actual SQL String got from an {@link SqlSource} after having processed any dynamic content. The SQL may have SQL
+ * placeholders "?" and an list (ordered) of an parameter mappings with the additional information for each parameter
+ * (at least the property name of the input object to read the value from). </br>
  * Can also have additional parameters that are created by the dynamic language (for loops, bind...).
  *
  * @author Clinton Begin
  */
 public class BoundSql {
 
-  private final String sql;
-  private final List<ParameterMapping> parameterMappings;
-  private final Object parameterObject;
-  private final Map<String, Object> additionalParameters;
-  private final MetaObject metaParameters;
+    private final String sql;
+    private final List<ParameterMapping> parameterMappings;
+    private final Object parameterObject;
+    private final Map<String, Object> additionalParameters;
+    private final MetaObject metaParameters;
+    private final List<Object> inParameterValues;
 
-  public BoundSql(Configuration configuration, String sql, List<ParameterMapping> parameterMappings, Object parameterObject) {
-    this.sql = sql;
-    this.parameterMappings = parameterMappings;
-    this.parameterObject = parameterObject;
-    this.additionalParameters = new HashMap<String, Object>();
-    this.metaParameters = configuration.newMetaObject(additionalParameters);
-  }
+    public BoundSql(Configuration configuration, String sql, List<ParameterMapping> parameterMappings,
+            Object parameterObject) {
+        this.sql = sql;
+        this.parameterMappings = parameterMappings;
+        this.parameterObject = parameterObject;
+        this.additionalParameters = new HashMap<String, Object>();
+        this.metaParameters = configuration.newMetaObject(additionalParameters);
+        this.inParameterValues = new ArrayList<Object>(parameterMappings != null ? parameterMappings.size() : 0);
 
-  public String getSql() {
-    return sql;
-  }
+    }
 
-  public List<ParameterMapping> getParameterMappings() {
-    return parameterMappings;
-  }
+    public void addInParameterValue(Object value) {
+        inParameterValues.add(value);
+    }
 
-  public Object getParameterObject() {
-    return parameterObject;
-  }
+    public Object getInParameterValues(int paramterIndex) {
+        return inParameterValues.get(paramterIndex);
+    }
 
-  public boolean hasAdditionalParameter(String name) {
-    String paramName = new PropertyTokenizer(name).getName();
-    return additionalParameters.containsKey(paramName);
-  }
+    public boolean isInParameterValueUnset() {
+        return inParameterValues != null && inParameterValues.isEmpty();
+    }
 
-  public void setAdditionalParameter(String name, Object value) {
-    metaParameters.setValue(name, value);
-  }
+    public String getSql() {
+        return sql;
+    }
 
-  public Object getAdditionalParameter(String name) {
-    return metaParameters.getValue(name);
-  }
+    public List<ParameterMapping> getParameterMappings() {
+        return parameterMappings;
+    }
+
+    public Object getParameterObject() {
+        return parameterObject;
+    }
+
+    public boolean hasAdditionalParameter(String name) {
+        String paramName = new PropertyTokenizer(name).getName();
+        return additionalParameters.containsKey(paramName);
+    }
+
+    public void setAdditionalParameter(String name, Object value) {
+        metaParameters.setValue(name, value);
+    }
+
+    public Object getAdditionalParameter(String name) {
+        return metaParameters.getValue(name);
+    }
 }
