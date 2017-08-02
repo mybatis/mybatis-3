cce4c2aabe1c99a6c4d40ae44e21ccb2b2e90192
cyflhn <cyflhn@163.com>
2017-08-03 06:29:37 +0800

    remove unrelated lines

--- src/main/java/org/apache/ibatis/scripting/defaults/DefaultParameterHandler.java
+++ src/main/java/org/apache/ibatis/scripting/defaults/DefaultParameterHandler.java
@@ -1,10 +1,17 @@
 /**
- * Copyright 2009-2017 the original author or authors. Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and limitations under the
- * License.
+ *    Copyright 2009-2017 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package org.apache.ibatis.scripting.defaults;
 
@@ -31,77 +38,68 @@ import org.apache.ibatis.type.TypeHandlerRegistry;
  */
 public class DefaultParameterHandler implements ParameterHandler {
 
-    private final TypeHandlerRegistry typeHandlerRegistry;
+  private final TypeHandlerRegistry typeHandlerRegistry;
 
-    private final MappedStatement mappedStatement;
-    private final Object parameterObject;
-    private final BoundSql boundSql;
-    private final Configuration configuration;
+  private final MappedStatement mappedStatement;
+  private final Object parameterObject;
+  private final BoundSql boundSql;
+  private final Configuration configuration;
 
-    public DefaultParameterHandler(MappedStatement mappedStatement, Object parameterObject, BoundSql boundSql) {
-        this.mappedStatement = mappedStatement;
-        this.configuration = mappedStatement.getConfiguration();
-        this.typeHandlerRegistry = mappedStatement.getConfiguration().getTypeHandlerRegistry();
-        this.parameterObject = parameterObject;
-        this.boundSql = boundSql;
-    }
+  public DefaultParameterHandler(MappedStatement mappedStatement, Object parameterObject, BoundSql boundSql) {
+    this.mappedStatement = mappedStatement;
+    this.configuration = mappedStatement.getConfiguration();
+    this.typeHandlerRegistry = mappedStatement.getConfiguration().getTypeHandlerRegistry();
+    this.parameterObject = parameterObject;
+    this.boundSql = boundSql;
+  }
 
-    @Override
-    public Object getParameterObject() {
-        return parameterObject;
-    }
+  @Override
+  public Object getParameterObject() {
+    return parameterObject;
+  }
 
-    @Override
-    public void setParameters(PreparedStatement ps) {
-        ErrorContext.instance().activity("setting parameters").object(mappedStatement.getParameterMap().getId());
-        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
-        if (parameterMappings != null) {
-            int inParamIndex = 0;
-            boolean inParameterValueUnset = boundSql.isInParameterValueUnset();
-            for (int i = 0; i < parameterMappings.size(); i++) {
-                ParameterMapping parameterMapping = parameterMappings.get(i);
-                if (parameterMapping.getMode() != ParameterMode.OUT) {
-                    Object value = null;
-                    if (inParameterValueUnset) {
-                        String propertyName = parameterMapping.getProperty();
-                        if (boundSql.hasAdditionalParameter(propertyName)) { // issue #448 ask first for additional
-                                                                             // params
-                            value = boundSql.getAdditionalParameter(propertyName);
-                        }
-                        else if (parameterObject == null) {
-                            value = null;
-                        }
-                        else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
-                            value = parameterObject;
-                        }
-                        else {
-                            MetaObject metaObject = configuration.newMetaObject(parameterObject);
-                            value = metaObject.getValue(propertyName);
-                        }
-                        boundSql.addInParameterValue(value);
-                    }
-                    else {
-                        value = boundSql.getInParameterValues(inParamIndex++);
-                    }
-                    TypeHandler typeHandler = parameterMapping.getTypeHandler();
-                    JdbcType jdbcType = parameterMapping.getJdbcType();
-                    if (value == null && jdbcType == null) {
-                        jdbcType = configuration.getJdbcTypeForNull();
-                    }
-                    try {
-                        typeHandler.setParameter(ps, i + 1, value, jdbcType);
-                    }
-                    catch (TypeException e) {
-                        throw new TypeException(
-                                "Could not set parameters for mapping: " + parameterMapping + ". Cause: " + e, e);
-                    }
-                    catch (SQLException e) {
-                        throw new TypeException(
-                                "Could not set parameters for mapping: " + parameterMapping + ". Cause: " + e, e);
-                    }
-                }
-            }
+  @Override
+  public void setParameters(PreparedStatement ps) {
+    ErrorContext.instance().activity("setting parameters").object(mappedStatement.getParameterMap().getId());
+    List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+    if (parameterMappings != null) {
+      boolean inParameterValueUnset = boundSql.isInParameterValueUnset();
+      int inParamIndex = 0;
+      for (int i = 0; i < parameterMappings.size(); i++) {
+        ParameterMapping parameterMapping = parameterMappings.get(i);
+        if (parameterMapping.getMode() != ParameterMode.OUT) {
+          Object value = null;
+          if (inParameterValueUnset) {
+              String propertyName = parameterMapping.getProperty();
+              if (boundSql.hasAdditionalParameter(propertyName)) { // issue #448 ask first for additional params
+                value = boundSql.getAdditionalParameter(propertyName);
+              } else if (parameterObject == null) {
+                value = null;
+              } else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                value = parameterObject;
+              } else {
+                MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                value = metaObject.getValue(propertyName);
+              }
+              boundSql.addInParameterValue(value);
+          }else{
+              value = boundSql.getInParameterValue(inParamIndex++);
+          }
+          TypeHandler typeHandler = parameterMapping.getTypeHandler();
+          JdbcType jdbcType = parameterMapping.getJdbcType();
+          if (value == null && jdbcType == null) {
+            jdbcType = configuration.getJdbcTypeForNull();
+          }
+          try {
+            typeHandler.setParameter(ps, i + 1, value, jdbcType);
+          } catch (TypeException e) {
+            throw new TypeException("Could not set parameters for mapping: " + parameterMapping + ". Cause: " + e, e);
+          } catch (SQLException e) {
+            throw new TypeException("Could not set parameters for mapping: " + parameterMapping + ". Cause: " + e, e);
+          }
         }
+      }
     }
+  }
 
 }
