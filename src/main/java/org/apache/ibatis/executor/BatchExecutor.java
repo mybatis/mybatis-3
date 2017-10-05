a38e44f2f1cc3f4067ec27616a7339abf2702f77
Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>
2017-10-05 16:57:01 +0200

    statement is closed immediately after execution

--- src/main/java/org/apache/ibatis/executor/BatchExecutor.java
+++ src/main/java/org/apache/ibatis/executor/BatchExecutor.java
@@ -131,6 +131,7 @@ public class BatchExecutor extends BaseExecutor {
               keyGenerator.processAfter(this, ms, stmt, parameter);
             }
           }
+          closeStatement(stmt);
         } catch (BatchUpdateException e) {
           StringBuilder message = new StringBuilder();
           message.append(batchResult.getMappedStatement().getId())
@@ -149,9 +150,6 @@ public class BatchExecutor extends BaseExecutor {
       }
       return results;
     } finally {
-      for (Statement stmt : statementList) {
-        closeStatement(stmt);
-      }
       currentSql = null;
       statementList.clear();
       batchResultList.clear();
