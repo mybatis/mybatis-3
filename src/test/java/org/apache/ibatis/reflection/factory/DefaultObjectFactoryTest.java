284379d5badb0accc8e18a5e00009391833f9df8
wei.zw <wei.zw@corp.netease.com>
2017-11-29 20:00:01 +0800

    change the method of instantiateClass from default to private and add test cases

--- src/test/java/org/apache/ibatis/reflection/factory/DefaultObjectFactoryTest.java
+++ src/test/java/org/apache/ibatis/reflection/factory/DefaultObjectFactoryTest.java
@@ -21,7 +21,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
