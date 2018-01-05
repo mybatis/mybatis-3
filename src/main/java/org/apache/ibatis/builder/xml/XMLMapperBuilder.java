21377c9e6b375938ab5d282bba071ef32da8621f
Iwao AVE! <harawata@gmail.com>
2018-01-05 22:50:11 +0900

    Trivial adjustment and license year updates.

--- src/main/java/org/apache/ibatis/builder/xml/XMLMapperBuilder.java
+++ src/main/java/org/apache/ibatis/builder/xml/XMLMapperBuilder.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
+ *    Copyright 2009-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -117,7 +117,7 @@ public class XMLMapperBuilder extends BaseBuilder {
       sqlElement(context.evalNodes("/mapper/sql"));
       buildStatementFromContext(context.evalNodes("select|insert|update|delete"));
     } catch (Exception e) {
-      throw new BuilderException("Error parsing Mapper XML.The XML location is " + resource + " Cause: " + e, e);
+      throw new BuilderException("Error parsing Mapper XML. The XML location is '" + resource + "'. Cause: " + e, e);
     }
   }
 
--- src/test/java/org/apache/ibatis/builder/XmlMapperBuilderTest.java
+++ src/test/java/org/apache/ibatis/builder/XmlMapperBuilderTest.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
+ *    Copyright 2009-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -175,7 +175,7 @@ public class XmlMapperBuilderTest {
   @Test
   public void shouldFailedLoadXMLMapperFile() throws Exception {
     expectedEx.expect(BuilderException.class);
-    expectedEx.expectMessage("Error parsing Mapper XML.The XML location is org/apache/ibatis/builder/ProblemMapper.xml");
+    expectedEx.expectMessage("Error parsing Mapper XML. The XML location is 'org/apache/ibatis/builder/ProblemMapper.xml'");
     Configuration configuration = new Configuration();
     String resource = "org/apache/ibatis/builder/ProblemMapper.xml";
     InputStream inputStream = Resources.getResourceAsStream(resource);
