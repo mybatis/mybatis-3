9a19aed74474cfb698152a3f7a4e673ee2b484bd
Loïc Mathieu <loikeseke@gmail.com>
2018-02-01 10:06:31 +0100

    Skip Object when collecting class methods (#1180)
    As a side effect, this might reduce the chance of 'illegal reflective access' warnings on Java 9. #1156

--- src/main/java/org/apache/ibatis/reflection/Reflector.java
+++ src/main/java/org/apache/ibatis/reflection/Reflector.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
+ *    Copyright 2009-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -313,7 +313,7 @@ public class Reflector {
   private Method[] getClassMethods(Class<?> cls) {
     Map<String, Method> uniqueMethods = new HashMap<String, Method>();
     Class<?> currentClass = cls;
-    while (currentClass != null) {
+    while (currentClass != null && currentClass != Object.class) {
       addUniqueMethods(uniqueMethods, currentClass.getDeclaredMethods());
 
       // we also need to look for interface methods -
