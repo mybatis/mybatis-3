24e9cb47c789891281303f9c78de19cf6468643a
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-03-20 07:37:30 +0900

    Upgrade to ognl 3.2.x #1211
    * Upgrade to ognl 3.2.5
    * Add OgnlMemberAccess
    * Avoid calling the deprecated method on OgnlCache

---
+++ src/main/java/org/apache/ibatis/scripting/xmltags/OgnlMemberAccess.java
@@ -0,0 +1,63 @@
+/**
+ *    Copyright 2009-2018 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.scripting.xmltags;
+
+import ognl.MemberAccess;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+
+/**
+ * The {@link MemberAccess} class that based on <a href=
+ * 'https://github.com/jkuhnert/ognl/blob/OGNL_3_2_1/src/java/ognl/DefaultMemberAccess.java'>DefaultMemberAccess</a>.
+ *
+ * @author Kazuki Shimizu
+ * @since 3.5.0
+ *
+ * @see <a href=
+ *      'https://github.com/jkuhnert/ognl/blob/OGNL_3_2_1/src/java/ognl/DefaultMemberAccess.java'>DefaultMemberAccess</a>
+ */
+class OgnlMemberAccess implements MemberAccess {
+
+  @Override
+  public Object setup(Map context, Object target, Member member, String propertyName) {
+    Object result = null;
+    if (isAccessible(context, target, member, propertyName)) {
+      AccessibleObject accessible = (AccessibleObject) member;
+      if (!accessible.isAccessible()) {
+        result = Boolean.FALSE;
+        accessible.setAccessible(true);
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public void restore(Map context, Object target, Member member, String propertyName,
+      Object state) {
+    if (state != null) {
+      ((AccessibleObject) member).setAccessible(((Boolean) state));
+    }
+  }
+
+  @Override
+  public boolean isAccessible(Map context, Object target, Member member, String propertyName) {
+    return Modifier.isPublic(member.getModifiers());
+  }
+
+}
