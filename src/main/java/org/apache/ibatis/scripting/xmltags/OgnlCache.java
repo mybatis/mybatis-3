25ce2db9d18e10c798db1e13694eb1ace0baf1d4
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-03-24 09:50:39 +0900

    Support concurrency on the OgnlClassResolver #1216

--- src/main/java/org/apache/ibatis/scripting/xmltags/OgnlCache.java
+++ src/main/java/org/apache/ibatis/scripting/xmltags/OgnlCache.java
@@ -33,6 +33,7 @@ import org.apache.ibatis.builder.BuilderException;
 public final class OgnlCache {
 
   private static final OgnlMemberAccess MEMBER_ACCESS = new OgnlMemberAccess();
+  private static final OgnlClassResolver CLASS_RESOLVER = new OgnlClassResolver();
   private static final Map<String, Object> expressionCache = new ConcurrentHashMap<String, Object>();
 
   private OgnlCache() {
@@ -41,7 +42,7 @@ public final class OgnlCache {
 
   public static Object getValue(String expression, Object root) {
     try {
-      Map context = Ognl.createDefaultContext(root, MEMBER_ACCESS, new OgnlClassResolver(), null);
+      Map context = Ognl.createDefaultContext(root, MEMBER_ACCESS, CLASS_RESOLVER, null);
       return Ognl.getValue(parseExpression(expression), context, root);
     } catch (OgnlException e) {
       throw new BuilderException("Error evaluating expression '" + expression + "'. Cause: " + e, e);
