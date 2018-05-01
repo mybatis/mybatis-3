a03332ffcb266e24bace846dab9cdc852d92f7df
huangzebin <huangzebin@wtoip.com>
2018-05-01 22:15:55 +0800

    增加RelectorFactory接口的中文注释

--- src/main/java/org/apache/ibatis/reflection/ReflectorFactory.java
+++ src/main/java/org/apache/ibatis/reflection/ReflectorFactory.java
@@ -1,25 +1,44 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection;
 
+/**
+ * 主要实现了对Reflector对象的创建和缓存
+ */
 public interface ReflectorFactory {
 
-  boolean isClassCacheEnabled();
+    /**
+     * 检测该ReflectorFactory对象是否会缓存Reflector对象
+     *
+     * @return
+     */
+    boolean isClassCacheEnabled();
 
-  void setClassCacheEnabled(boolean classCacheEnabled);
+    /**
+     * 设置是否缓存Reflector对象
+     *
+     * @param classCacheEnabled
+     */
+    void setClassCacheEnabled(boolean classCacheEnabled);
 
-  Reflector findForClass(Class<?> type);
+    /**
+     * 创建指定class对应的Reflector对象
+     *
+     * @param type
+     * @return
+     */
+    Reflector findForClass(Class<?> type);
 }
