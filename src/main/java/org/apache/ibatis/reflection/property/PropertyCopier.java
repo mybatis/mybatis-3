f772a9eeb564c533118b738e4f2623015addfcb9
huangzebin <huangzebin@wtoip.com>
2018-05-09 21:50:19 +0800

    完成PropertyCopier的分析

--- src/main/java/org/apache/ibatis/reflection/property/PropertyCopier.java
+++ src/main/java/org/apache/ibatis/reflection/property/PropertyCopier.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.property;
 
@@ -22,24 +22,32 @@ import java.lang.reflect.Field;
  */
 public final class PropertyCopier {
 
-  private PropertyCopier() {
-    // Prevent Instantiation of Static Class
-  }
-
-  public static void copyBeanProperties(Class<?> type, Object sourceBean, Object destinationBean) {
-    Class<?> parent = type;
-    while (parent != null) {
-      final Field[] fields = parent.getDeclaredFields();
-      for(Field field : fields) {
-        try {
-          field.setAccessible(true);
-          field.set(destinationBean, field.get(sourceBean));
-        } catch (Exception e) {
-          // Nothing useful to do, will only fail on final fields, which will be ignored.
-        }
-      }
-      parent = parent.getSuperclass();
+    private PropertyCopier() {
+        // Prevent Instantiation of Static Class
     }
-  }
+
+    /**
+     * 相同类型的两个对象之间的属性值拷贝
+     *
+     * @param type            类型
+     * @param sourceBean      源对象
+     * @param destinationBean 目标对象
+     */
+    public static void copyBeanProperties(Class<?> type, Object sourceBean, Object destinationBean) {
+        Class<?> parent = type;
+        while (parent != null) {
+            final Field[] fields = parent.getDeclaredFields();
+            for (Field field : fields) {
+                try {
+                    field.setAccessible(true);
+                    // 将sourceBean中的属性值，设置到destinationBean对应的属性中
+                    field.set(destinationBean, field.get(sourceBean));
+                } catch (Exception e) {
+                    // Nothing useful to do, will only fail on final fields, which will be ignored.
+                }
+            }
+            parent = parent.getSuperclass();
+        }
+    }
 
 }
--- src/main/java/org/apache/ibatis/reflection/property/PropertyNamer.java
+++ src/main/java/org/apache/ibatis/reflection/property/PropertyNamer.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.property;
 
@@ -24,36 +24,42 @@ import org.apache.ibatis.reflection.ReflectionException;
  */
 public final class PropertyNamer {
 
-  private PropertyNamer() {
-    // Prevent Instantiation of Static Class
-  }
-
-  public static String methodToProperty(String name) {
-    if (name.startsWith("is")) {
-      name = name.substring(2);
-    } else if (name.startsWith("get") || name.startsWith("set")) {
-      name = name.substring(3);
-    } else {
-      throw new ReflectionException("Error parsing property name '" + name + "'.  Didn't start with 'is', 'get' or 'set'.");
+    private PropertyNamer() {
+        // Prevent Instantiation of Static Class
     }
 
-    if (name.length() == 1 || (name.length() > 1 && !Character.isUpperCase(name.charAt(1)))) {
-      name = name.substring(0, 1).toLowerCase(Locale.ENGLISH) + name.substring(1);
+    /**
+     * 将方法明转化为属性名称
+     *
+     * @param name 方法名称
+     * @return
+     */
+    public static String methodToProperty(String name) {
+        if (name.startsWith("is")) {
+            name = name.substring(2);
+        } else if (name.startsWith("get") || name.startsWith("set")) {
+            name = name.substring(3);
+        } else {
+            throw new ReflectionException("Error parsing property name '" + name + "'.  Didn't start with 'is', 'get' or 'set'.");
+        }
+
+        if (name.length() == 1 || (name.length() > 1 && !Character.isUpperCase(name.charAt(1)))) {
+            name = name.substring(0, 1).toLowerCase(Locale.ENGLISH) + name.substring(1);
+        }
+
+        return name;
     }
 
-    return name;
-  }
+    public static boolean isProperty(String name) {
+        return name.startsWith("get") || name.startsWith("set") || name.startsWith("is");
+    }
 
-  public static boolean isProperty(String name) {
-    return name.startsWith("get") || name.startsWith("set") || name.startsWith("is");
-  }
+    public static boolean isGetter(String name) {
+        return name.startsWith("get") || name.startsWith("is");
+    }
 
-  public static boolean isGetter(String name) {
-    return name.startsWith("get") || name.startsWith("is");
-  }
-
-  public static boolean isSetter(String name) {
-    return name.startsWith("set");
-  }
+    public static boolean isSetter(String name) {
+        return name.startsWith("set");
+    }
 
 }
