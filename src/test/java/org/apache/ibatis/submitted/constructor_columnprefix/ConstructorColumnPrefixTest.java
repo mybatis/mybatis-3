058d6d15d433d2b87a5dae704293d82f208d90c5
Iwao AVE! <harawata@gmail.com>
2018-07-27 01:36:55 +0900

    Add `columnPrefix` to `@Arg` as well.

--- src/test/java/org/apache/ibatis/submitted/constructor_columnprefix/ConstructorColumnPrefixTest.java
+++ src/test/java/org/apache/ibatis/submitted/constructor_columnprefix/ConstructorColumnPrefixTest.java
@@ -50,18 +50,31 @@ public class ConstructorColumnPrefixTest {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       Mapper mapper = sqlSession.getMapper(Mapper.class);
       List<Article> articles = mapper.getArticles();
-      assertEquals(2, articles.size());
-      Article article1 = articles.get(0);
-      assertEquals(Integer.valueOf(1), article1.getId().getId());
-      assertEquals("Article 1", article1.getName());
-      assertEquals("Mary", article1.getAuthor().getName());
-      assertEquals("Bob", article1.getCoauthor().getName());
-      Article article2 = articles.get(1);
-      assertEquals(Integer.valueOf(2), article2.getId().getId());
-      assertEquals("Article 2", article2.getName());
-      assertEquals("Jane", article2.getAuthor().getName());
-      assertEquals("Mary", article2.getCoauthor().getName());
+      assertArticles(articles);
     }
+  }
+
+  @Test
+  public void shouldGetArticlesAnno() {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      Mapper mapper = sqlSession.getMapper(Mapper.class);
+      List<Article> articles = mapper.getArticlesAnno();
+      assertArticles(articles);
+    }
+  }
+
+  protected void assertArticles(List<Article> articles) {
+    assertEquals(2, articles.size());
+    Article article1 = articles.get(0);
+    assertEquals(Integer.valueOf(1), article1.getId().getId());
+    assertEquals("Article 1", article1.getName());
+    assertEquals("Mary", article1.getAuthor().getName());
+    assertEquals("Bob", article1.getCoauthor().getName());
+    Article article2 = articles.get(1);
+    assertEquals(Integer.valueOf(2), article2.getId().getId());
+    assertEquals("Article 2", article2.getName());
+    assertEquals("Jane", article2.getAuthor().getName());
+    assertEquals("Mary", article2.getCoauthor().getName());
   }
 
 }
--- src/test/java/org/apache/ibatis/submitted/constructor_columnprefix/Mapper.java
+++ src/test/java/org/apache/ibatis/submitted/constructor_columnprefix/Mapper.java
@@ -17,8 +17,28 @@ package org.apache.ibatis.submitted.constructor_columnprefix;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Arg;
+import org.apache.ibatis.annotations.ConstructorArgs;
+import org.apache.ibatis.annotations.Select;
+
 public interface Mapper {
 
   List<Article> getArticles();
+
+  @ConstructorArgs({
+      @Arg(id = true, resultMap = "keyRM", columnPrefix = "key_", javaType = EntityKey.class),
+      @Arg(column = "name", javaType = String.class),
+      @Arg(resultMap = "authorRM", columnPrefix = "author_", javaType = Author.class),
+      @Arg(resultMap = "authorRM", columnPrefix = "coauthor_", javaType = Author.class),
+  })
+  @Select({
+      "select id key_id, name, author.id author_id, author.name author_name,",
+      "  coauthor.id coauthor_id, coauthor.name coauthor_name",
+      "from articles",
+      "left join authors author on author.id = articles.author_id",
+      "left join authors coauthor on coauthor.id = articles.coauthor_id",
+      "order by articles.id"
+  })
+  List<Article> getArticlesAnno();
 
 }
