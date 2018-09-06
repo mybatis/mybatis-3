af0016aa06592317003ce65f0c4b2f43d466dd86
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-09-06 22:32:23 +0900

    Polishing #1334

--- src/test/java/org/apache/ibatis/submitted/result_set_type/CreateDB.sql
+++ src/test/java/org/apache/ibatis/submitted/result_set_type/CreateDB.sql
@@ -14,43 +14,40 @@
 --    limitations under the License.
 --
 
-drop table resultsettype.users if exists
+drop table users if exists
 go
 
-drop procedure resultsettype.getusers if exists
+drop procedure getusers if exists
 go
 
-create schema resultsettype
-go
-
-create table resultsettype.users (
+create table users (
   id int,
   name varchar(20)
 )
 go
 
-create procedure resultsettype.getusers()
+create procedure getusers()
 modifies sql data
 dynamic result sets 1
 BEGIN ATOMIC
-  declare cur cursor for select * from resultsettype.users order by id;
+  declare cur cursor for select * from users order by id;
   open cur;
 END
 go
 
-insert into resultsettype.users (id, name) values(1, 'User1')
+insert into users (id, name) values(1, 'User1')
 go
 
-insert into resultsettype.users (id, name) values(2, 'User1')
+insert into users (id, name) values(2, 'User1')
 go
 
-insert into resultsettype.users (id, name) values(3, 'User1')
+insert into users (id, name) values(3, 'User1')
 go
 
-insert into resultsettype.users (id, name) values(4, 'User1')
+insert into users (id, name) values(4, 'User1')
 go
 
-insert into resultsettype.users (id, name) values(5, 'User1')
+insert into users (id, name) values(5, 'User1')
 go
 
 
