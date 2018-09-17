261d212814c7dd5adc141b9f766ab19def47bedd
kezhenxu94 <kezhenxu94@163.com>
2018-09-17 23:41:24 +0800

    Fix bug #1357

--- src/test/java/org/apache/ibatis/cache/BlockingCacheTest.java
+++ src/test/java/org/apache/ibatis/cache/BlockingCacheTest.java
@@ -2,6 +2,7 @@ package org.apache.ibatis.cache;
 
 import org.apache.ibatis.cache.decorators.BlockingCache;
 import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -19,7 +20,7 @@ public class BlockingCacheTest {
         cache.getObject(1);
         cache.getObject(1);
 
-        Thread thread = new Thread(() -> System.out.println(cache.getObject(1)));
+        Thread thread = new Thread(() -> Assert.assertEquals(cache.getObject(1), 1));
         thread.setDaemon(false);
         thread.start();
 
