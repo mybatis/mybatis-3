56b91e5a90e0d6154774e4cccba24a39efd408e7
Iwao AVE! <harawata@gmail.com>
2019-01-03 01:41:55 +0900

    Resolve modernizer error.

--- src/test/java/org/apache/ibatis/datasource/jndi/JndiDataSourceFactoryTest.java
+++ src/test/java/org/apache/ibatis/datasource/jndi/JndiDataSourceFactoryTest.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
+ *    Copyright 2009-2019 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ public class JndiDataSourceFactoryTest extends BaseDataTest {
 
   private void createJndiDataSource() throws Exception {
     try {
-      Hashtable<String, String> env = new Hashtable<String, String>();
+      Properties env = new Properties();
       env.put(Context.INITIAL_CONTEXT_FACTORY, TEST_INITIAL_CONTEXT_FACTORY);
 
       MockContext ctx = new MockContext(false);
