89aaefb422f4dee54a4e13682811a1645f88a511
Helly Guo <buffoonguo@gmail.com>
2019-01-12 09:11:50 +0800

    [enhance] use the enum StatementType to skip the type detection

--- src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
+++ src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
@@ -15,19 +15,18 @@
  */
 package org.apache.ibatis.executor.statement;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
-
 import org.apache.ibatis.cursor.Cursor;
 import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.executor.ExecutorException;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 
 /**
  * @author Clinton Begin
@@ -38,19 +37,7 @@ public class RoutingStatementHandler implements StatementHandler {
 
   public RoutingStatementHandler(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) {
 
-    switch (ms.getStatementType()) {
-      case STATEMENT:
-        delegate = new SimpleStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
-        break;
-      case PREPARED:
-        delegate = new PreparedStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
-        break;
-      case CALLABLE:
-        delegate = new CallableStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
-        break;
-      default:
-        throw new ExecutorException("Unknown statement type: " + ms.getStatementType());
-    }
+    delegate = ms.getStatementType().createHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
 
   }
 
--- src/main/java/org/apache/ibatis/mapping/StatementType.java
+++ src/main/java/org/apache/ibatis/mapping/StatementType.java
@@ -15,9 +15,35 @@
  */
 package org.apache.ibatis.mapping;
 
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.statement.CallableStatementHandler;
+import org.apache.ibatis.executor.statement.PreparedStatementHandler;
+import org.apache.ibatis.executor.statement.SimpleStatementHandler;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
 /**
  * @author Clinton Begin
  */
 public enum StatementType {
-  STATEMENT, PREPARED, CALLABLE
+  STATEMENT(SimpleStatementHandler::new), PREPARED(PreparedStatementHandler::new), CALLABLE(CallableStatementHandler::new);
+
+  private StatementHandlerCreator creator;
+
+  StatementType(StatementHandlerCreator creator){
+    this.creator = creator;
+  }
+
+  public StatementHandler createHandler(Executor executor, MappedStatement ms,
+                                        Object parameter, RowBounds rowBounds,
+                                        ResultHandler resultHandler, BoundSql boundSql){
+    return creator.create(executor, ms, parameter, rowBounds, resultHandler, boundSql);
+  }
+
+  private interface StatementHandlerCreator{
+    StatementHandler create(Executor executor, MappedStatement ms,
+                            Object parameter, RowBounds rowBounds,
+                            ResultHandler resultHandler, BoundSql boundSql);
+  }
 }
