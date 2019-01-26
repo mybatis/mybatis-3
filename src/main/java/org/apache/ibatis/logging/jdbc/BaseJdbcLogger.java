3385a4d573124b75feda1313190bfdd0bf665db8
Kazuki Shimizu <kazuki43zoo@gmail.com>
2019-01-26 21:19:14 +0900

    Change way that define a setter method to bind to PreparedStatement
    * To use reflection
    
    In this changes, following methods are support.
    
    * setRef
    * setRowId
    * setSQLXML
    * setUnicodeStream
    * setURL

--- src/main/java/org/apache/ibatis/logging/jdbc/BaseJdbcLogger.java
+++ src/main/java/org/apache/ibatis/logging/jdbc/BaseJdbcLogger.java
@@ -15,7 +15,9 @@
  */
 package org.apache.ibatis.logging.jdbc;
 
+import java.lang.reflect.Method;
 import java.sql.Array;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.reflection.ArrayUtil;
@@ -37,7 +40,7 @@ import org.apache.ibatis.reflection.ArrayUtil;
  */
 public abstract class BaseJdbcLogger {
 
-  protected static final Set<String> SET_METHODS = new HashSet<>();
+  protected static final Set<String> SET_METHODS;
   protected static final Set<String> EXECUTE_METHODS = new HashSet<>();
 
   private final Map<Object, Object> columnMap = new HashMap<>();
@@ -61,30 +64,11 @@ public abstract class BaseJdbcLogger {
   }
 
   static {
-    SET_METHODS.add("setString");
-    SET_METHODS.add("setNString");
-    SET_METHODS.add("setInt");
-    SET_METHODS.add("setByte");
-    SET_METHODS.add("setShort");
-    SET_METHODS.add("setLong");
-    SET_METHODS.add("setDouble");
-    SET_METHODS.add("setFloat");
-    SET_METHODS.add("setTimestamp");
-    SET_METHODS.add("setDate");
-    SET_METHODS.add("setTime");
-    SET_METHODS.add("setArray");
-    SET_METHODS.add("setBigDecimal");
-    SET_METHODS.add("setAsciiStream");
-    SET_METHODS.add("setBinaryStream");
-    SET_METHODS.add("setBlob");
-    SET_METHODS.add("setBoolean");
-    SET_METHODS.add("setBytes");
-    SET_METHODS.add("setCharacterStream");
-    SET_METHODS.add("setNCharacterStream");
-    SET_METHODS.add("setClob");
-    SET_METHODS.add("setNClob");
-    SET_METHODS.add("setObject");
-    SET_METHODS.add("setNull");
+    SET_METHODS = Arrays.stream(PreparedStatement.class.getDeclaredMethods())
+            .filter(method -> method.getName().startsWith("set"))
+            .filter(method -> method.getParameterCount() > 1)
+            .map(Method::getName)
+            .collect(Collectors.toSet());
 
     EXECUTE_METHODS.add("execute");
     EXECUTE_METHODS.add("executeUpdate");
