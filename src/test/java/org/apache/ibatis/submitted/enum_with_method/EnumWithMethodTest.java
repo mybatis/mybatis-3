51710ed8edd0f052ef395f848e0b235a1a259afc
ZHOU Zehua <zhouzehua@outlook.com>
2019-03-04 16:28:41 +0800

    add a test to reproduce problem of enum type handler
    MyBatis cannot find the type handler for an enum when 1. the enum value has
    methods, 2. no parameterType is provided in corresponding insert mappers

---
+++ src/test/java/org/apache/ibatis/submitted/enum_with_method/EnumWithMethodTest.java
@@ -0,0 +1,67 @@
+/*
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.submitted.enum_with_method;
+
+import java.io.Reader;
+
+import org.apache.ibatis.BaseDataTest;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class EnumWithMethodTest {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        // create an SqlSessionFactory
+        try (Reader reader = Resources
+                .getResourceAsReader("org/apache/ibatis/submitted/enum_with_method/mybatis-config.xml")) {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        }
+
+        // populate in-memory database
+        BaseDataTest.runScript(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource(),
+                "org/apache/ibatis/submitted/enum_with_method/CreateDB.sql");
+    }
+
+    @Test
+    void shouldGetAUser() {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            Mapper mapper = sqlSession.getMapper(Mapper.class);
+            User user = mapper.getUser(1);
+            Assertions.assertEquals("User1", user.getName());
+        }
+    }
+
+    @Test
+    void shouldInsertAUser() {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            Mapper mapper = sqlSession.getMapper(Mapper.class);
+            User user = new User();
+            user.setId(2);
+            user.setName("User2");
+            user.setCur(Currency.Dollar);
+            mapper.insertUser(user);
+        }
+    }
+
+}
