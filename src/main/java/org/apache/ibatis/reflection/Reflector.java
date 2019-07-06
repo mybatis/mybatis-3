c9d829f880271055be34e57664f304917e778d39
cp_hdu@163.com <cp_hdu@163.com>
2019-07-06 09:56:40 +0800

    string -> char, update license year.

--- src/main/java/org/apache/ibatis/reflection/Reflector.java
+++ src/main/java/org/apache/ibatis/reflection/Reflector.java
@@ -15,8 +15,22 @@
  */
 package org.apache.ibatis.reflection;
 
-import java.lang.reflect.*;
-import java.util.*;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.ReflectPermission;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.ibatis.reflection.invoker.GetFieldInvoker;
@@ -299,7 +313,7 @@ public class Reflector {
     sb.append(method.getName());
     Class<?>[] parameters = method.getParameterTypes();
     for (int i = 0; i < parameters.length; i++) {
-      sb.append(i == 0 ? ":" : ",").append(parameters[i].getName());
+      sb.append(i == 0 ? ':' : ',').append(parameters[i].getName());
     }
     return sb.toString();
   }
