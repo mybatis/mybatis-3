bf5db14351ccdc2992410acab2663bd48a1378b5
Iwao AVE! <harawata@gmail.com>
2019-09-27 22:16:42 +0900

    Updated license header and reformatted

--- src/main/java/org/apache/ibatis/executor/statement/StatementUtil.java
+++ src/main/java/org/apache/ibatis/executor/statement/StatementUtil.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
+ *    Copyright 2009-2019 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ public class StatementUtil {
    */
   public static void applyTransactionTimeout(Statement statement, Integer queryTimeout, Integer transactionTimeout) throws SQLException {
     if (transactionTimeout == null) {
-        return;
+      return;
     }
     if (queryTimeout == null || queryTimeout == 0 || transactionTimeout < queryTimeout) {
       statement.setQueryTimeout(transactionTimeout);
