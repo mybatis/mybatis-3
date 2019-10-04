0325bd483cfd3bba3bdfec72dc52a779d73d6e8e
GODZHL <zhanghanlinhs@163.com>
2019-10-04 23:56:22 +0800

    Modify duplicate code

--- src/main/java/org/apache/ibatis/logging/jdbc/ConnectionLogger.java
+++ src/main/java/org/apache/ibatis/logging/jdbc/ConnectionLogger.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.logging.jdbc;
 
@@ -27,72 +27,65 @@ import org.apache.ibatis.reflection.ExceptionUtil;
 
 /**
  * Connection proxy to add logging
- * 
+ *
  * @author Clinton Begin
  * @author Eduardo Macarron
- * 
+ *
  */
 public final class ConnectionLogger extends BaseJdbcLogger implements InvocationHandler {
 
-  private final Connection connection;
+    private final Connection connection;
 
-  private ConnectionLogger(Connection conn, Log statementLog, int queryStack) {
-    super(statementLog, queryStack);
-    this.connection = conn;
-  }
-
-  @Override
-  public Object invoke(Object proxy, Method method, Object[] params)
-      throws Throwable {
-    try {
-      if (Object.class.equals(method.getDeclaringClass())) {
-        return method.invoke(this, params);
-      }    
-      if ("prepareStatement".equals(method.getName())) {
-        if (isDebugEnabled()) {
-          debug(" Preparing: " + removeBreakingWhitespace((String) params[0]), true);
-        }        
-        PreparedStatement stmt = (PreparedStatement) method.invoke(connection, params);
-        stmt = PreparedStatementLogger.newInstance(stmt, statementLog, queryStack);
-        return stmt;
-      } else if ("prepareCall".equals(method.getName())) {
-        if (isDebugEnabled()) {
-          debug(" Preparing: " + removeBreakingWhitespace((String) params[0]), true);
-        }        
-        PreparedStatement stmt = (PreparedStatement) method.invoke(connection, params);
-        stmt = PreparedStatementLogger.newInstance(stmt, statementLog, queryStack);
-        return stmt;
-      } else if ("createStatement".equals(method.getName())) {
-        Statement stmt = (Statement) method.invoke(connection, params);
-        stmt = StatementLogger.newInstance(stmt, statementLog, queryStack);
-        return stmt;
-      } else {
-        return method.invoke(connection, params);
-      }
-    } catch (Throwable t) {
-      throw ExceptionUtil.unwrapThrowable(t);
+    private ConnectionLogger(Connection conn, Log statementLog, int queryStack) {
+        super(statementLog, queryStack);
+        this.connection = conn;
     }
-  }
 
-  /**
-   * Creates a logging version of a connection
-   *
-   * @param conn - the original connection
-   * @return - the connection with logging
-   */
-  public static Connection newInstance(Connection conn, Log statementLog, int queryStack) {
-    InvocationHandler handler = new ConnectionLogger(conn, statementLog, queryStack);
-    ClassLoader cl = Connection.class.getClassLoader();
-    return (Connection) Proxy.newProxyInstance(cl, new Class[]{Connection.class}, handler);
-  }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] params)
+            throws Throwable {
+        try {
+            if (Object.class.equals(method.getDeclaringClass())) {
+                return method.invoke(this, params);
+            }
+            if ("prepareStatement".equals(method.getName()) || "prepareCall".equals(method.getName())) {
+                if (isDebugEnabled()) {
+                    debug(" Preparing: " + removeBreakingWhitespace((String) params[0]), true);
+                }
+                PreparedStatement stmt = (PreparedStatement) method.invoke(connection, params);
+                stmt = PreparedStatementLogger.newInstance(stmt, statementLog, queryStack);
+                return stmt;
+            } else if ("createStatement".equals(method.getName())) {
+                Statement stmt = (Statement) method.invoke(connection, params);
+                stmt = StatementLogger.newInstance(stmt, statementLog, queryStack);
+                return stmt;
+            } else {
+                return method.invoke(connection, params);
+            }
+        } catch (Throwable t) {
+            throw ExceptionUtil.unwrapThrowable(t);
+        }
+    }
 
-  /**
-   * return the wrapped connection
-   *
-   * @return the connection
-   */
-  public Connection getConnection() {
-    return connection;
-  }
+    /**
+     * Creates a logging version of a connection
+     *
+     * @param conn - the original connection
+     * @return - the connection with logging
+     */
+    public static Connection newInstance(Connection conn, Log statementLog, int queryStack) {
+        InvocationHandler handler = new ConnectionLogger(conn, statementLog, queryStack);
+        ClassLoader cl = Connection.class.getClassLoader();
+        return (Connection) Proxy.newProxyInstance(cl, new Class[]{Connection.class}, handler);
+    }
+
+    /**
+     * return the wrapped connection
+     *
+     * @return the connection
+     */
+    public Connection getConnection() {
+        return connection;
+    }
 
 }
