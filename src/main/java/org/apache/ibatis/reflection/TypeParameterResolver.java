7c06b256c6fac1bfba5f06a40fb759decfe145e5
Iwao AVE! <harawata@gmail.com>
2019-12-31 23:02:55 +0900

    Fix for #1794 Comparing TypeVariable must be done by `equals()`

--- src/main/java/org/apache/ibatis/reflection/TypeParameterResolver.java
+++ src/main/java/org/apache/ibatis/reflection/TypeParameterResolver.java
@@ -179,7 +179,7 @@ public class TypeParameterResolver {
       }
       if (declaringClass == parentAsClass) {
         for (int i = 0; i < parentTypeVars.length; i++) {
-          if (typeVar == parentTypeVars[i]) {
+          if (typeVar.equals(parentTypeVars[i])) {
             return parentAsType.getActualTypeArguments()[i];
           }
         }
@@ -202,7 +202,7 @@ public class TypeParameterResolver {
     for (int i = 0; i < parentTypeArgs.length; i++) {
       if (parentTypeArgs[i] instanceof TypeVariable) {
         for (int j = 0; j < srcTypeVars.length; j++) {
-          if (srcTypeVars[j] == parentTypeArgs[i]) {
+          if (srcTypeVars[j].equals(parentTypeArgs[i])) {
             noChange = false;
             newParentArgs[i] = srcTypeArgs[j];
           }
