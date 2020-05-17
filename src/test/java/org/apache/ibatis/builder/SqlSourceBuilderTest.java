08635fd46fc1b71e62811dd4951eff6480459107
heegu.lee <elfhazard@gmail.com>
2020-05-17 15:11:53 +0900

    test case code refactoring (https://github.com/mybatis/mybatis-3/pull/1901)

--- src/test/java/org/apache/ibatis/builder/SqlSourceBuilderTest.java
+++ src/test/java/org/apache/ibatis/builder/SqlSourceBuilderTest.java
@@ -15,30 +15,22 @@
  */
 package org.apache.ibatis.builder;
 
-import java.io.Reader;
-import org.apache.ibatis.builder.xml.XMLConfigBuilder;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.SqlSource;
 import org.apache.ibatis.session.Configuration;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SqlSourceBuilderTest {
 
   private static Configuration configuration;
   private static SqlSourceBuilder sqlSourceBuilder;
-  String sqlFromXml = "SELECT * \n        FROM user\n        WHERE user_id = 1";
+  String sqlFromXml = "\\t\\n\\n  SELECT * \\n        FROM user\\n \\t        WHERE user_id = 1\\n\\t  ";
 
-  @BeforeAll
-  static void setUp() throws Exception {
-    // create an SqlSessionFactory
-    try (Reader reader = Resources
-      .getResourceAsReader("org/apache/ibatis/submitted/empty_row/mybatis-config.xml")) {
-      XMLConfigBuilder parser = new XMLConfigBuilder(reader, null, null);
-      configuration = parser.parse();
-    }
+  @BeforeEach
+  void setUp() {
+    configuration = new Configuration();
 
     sqlSourceBuilder = new SqlSourceBuilder(configuration);
   }
@@ -53,10 +45,13 @@ public class SqlSourceBuilderTest {
 
   @Test
   void testMinifySqlEnabledIsTrue() {
+    String expected = "\\t\\n\\n SELECT * \\n FROM user\\n \\t WHERE user_id = 1\\n\\t";
+
     configuration.setShrinkWhitespacesInSql(true);
     SqlSource sqlSource = sqlSourceBuilder.parse(sqlFromXml, null, null);
     BoundSql boundSql = sqlSource.getBoundSql(null);
     String actual = boundSql.getSql();
-    Assertions.assertNotEquals(sqlFromXml, actual);
+
+    Assertions.assertEquals(expected, actual);
   }
 }
