fc3401bb69e98d01aca8e05aa398c903bfbb45e4
Iwao AVE! <harawata@gmail.com>
2020-09-08 22:08:09 +0900

    Added a note about the deadlock risk

--- src/main/java/org/apache/ibatis/cache/decorators/BlockingCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/BlockingCache.java
@@ -24,11 +24,13 @@ import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.cache.CacheException;
 
 /**
- * Simple blocking decorator
+ * <p>Simple blocking decorator
  *
- * Simple and inefficient version of EhCache's BlockingCache decorator.
+ * <p>Simple and inefficient version of EhCache's BlockingCache decorator.
  * It sets a lock over a cache key when the element is not found in cache.
  * This way, other threads will wait until this element is filled instead of hitting the database.
+ *
+ * <p>By its nature, this implementation can cause deadlock when used incorrecly.
  *
  * @author Eduardo Macarron
  *
