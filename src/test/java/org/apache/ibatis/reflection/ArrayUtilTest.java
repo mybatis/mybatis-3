6979b56d7d8e3317dbc251719d69c80826f318f8
lili <xiaozhiliaoo@gmail.com>
2020-11-19 11:00:04 +0800

    fix(test) :ArrayUtilTest should not be extends ArrayUtil

--- src/test/java/org/apache/ibatis/reflection/ArrayUtilTest.java
+++ src/test/java/org/apache/ibatis/reflection/ArrayUtilTest.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
+ *    Copyright 2009-2020 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-class ArrayUtilTest extends ArrayUtil {
+class ArrayUtilTest  {
 
   @Test
   void testHashCode() {
@@ -51,7 +51,7 @@ class ArrayUtilTest extends ArrayUtil {
   }
 
   @Test
-  void testequals() {
+  void testEquals() {
     assertTrue(ArrayUtil.equals(new long[] { 1 }, new long[] { 1 }));
     assertTrue(ArrayUtil.equals(new int[] { 1 }, new int[] { 1 }));
     assertTrue(ArrayUtil.equals(new short[] { 1 }, new short[] { 1 }));
