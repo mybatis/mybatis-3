239cdaec588b92ce7cb36e99266c827d1810d67f
wuxinjie1 <wuxinjie1@yiche.com>
2021-01-22 16:59:57 +0800

    chore: optimize code

--- src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
+++ src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
@@ -396,12 +396,11 @@ public class XMLConfigBuilder extends BaseBuilder {
   private boolean isSpecifiedEnvironment(String id) {
     if (environment == null) {
       throw new BuilderException("No environment specified.");
-    } else if (id == null) {
-      throw new BuilderException("Environment requires an id attribute.");
-    } else if (environment.equals(id)) {
-      return true;
     }
-    return false;
+    if (id == null) {
+      throw new BuilderException("Environment requires an id attribute.");
+    }
+    return environment.equals(id);
   }
 
 }
