af3908c88bf9e0f34713c589942f82c22aaf4c9e
咩咩 <469517790@qq.com>
2021-03-31 18:53:00 +0800

    test case

--- src/test/java/org/apache/ibatis/autoconstructor/CreateDB.sql
+++ src/test/java/org/apache/ibatis/autoconstructor/CreateDB.sql
@@ -26,9 +26,9 @@ CREATE TABLE subject (
   age    INT NOT NULL,
   height INT,
   weight INT,
-  width INT,
   active BIT,
-  dt     TIMESTAMP
+  dt     TIMESTAMP,
+  width INT
 );
 
 CREATE TABLE extensive_subject (
@@ -48,9 +48,9 @@ CREATE TABLE extensive_subject (
 );
 
 INSERT INTO subject VALUES
-  (1, 'a', 10, 100, 45, 50, 1, CURRENT_TIMESTAMP),
-  (2, 'b', 10, NULL, 45, 50, 1, CURRENT_TIMESTAMP),
-  (2, 'c', 10, NULL, NULL,Null, 0, CURRENT_TIMESTAMP);
+  (1, 'a', 10, 100, 45, 1, CURRENT_TIMESTAMP, 50),
+  (2, 'b', 10, NULL, 45, 1, CURRENT_TIMESTAMP, 50),
+  (2, 'c', 10, NULL, NULL, 0, CURRENT_TIMESTAMP, 50);
 
 INSERT INTO extensive_subject
 VALUES
