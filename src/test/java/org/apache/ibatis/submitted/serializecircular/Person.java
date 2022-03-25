67d8b033a1d4f136f5ca8a36699ceb5365a81667
tasnim <tasnim@dal.ca>
2022-03-25 00:29:54 -0300

    Change bidirectional association to unidirectional association

--- src/test/java/org/apache/ibatis/submitted/serializecircular/Person.java
+++ src/test/java/org/apache/ibatis/submitted/serializecircular/Person.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009-2021 the original author or authors.
+ *    Copyright 2009-2022 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -18,18 +18,10 @@ package org.apache.ibatis.submitted.serializecircular;
 import java.io.Serializable;
 
 public class Person implements Serializable {
+
   private static final long serialVersionUID = 1L;
 
   private Integer id;
-  private Department department;
-
-  public Department getDepartment() {
-    return department;
-  }
-
-  public void setDepartment(Department department) {
-    this.department = department;
-  }
 
   public Integer getId() {
     return id;
