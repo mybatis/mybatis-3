ab4d1e21ae28c8d4fbe5af9ca2a863afe6522161
Jeremy Landis <jeremylandis@hotmail.com>
2022-08-24 22:22:49 -0400

    [mvn] Update maven wrapper

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -17,14 +17,24 @@
  * under the License.
  */
 
-import java.net.*;
-import java.io.*;
-import java.nio.channels.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.Properties;
 
-public class MavenWrapperDownloader
+public final class MavenWrapperDownloader
 {
-    private static final String WRAPPER_VERSION = "3.1.0";
+    private static final String WRAPPER_VERSION = "3.1.1";
+
+    private static final boolean VERBOSE = Boolean.parseBoolean( System.getenv( "MVNW_VERBOSE" ) );
 
     /**
      * Default URL to download the maven-wrapper.jar from, if no 'downloadUrl' is provided.
@@ -49,78 +59,53 @@ public class MavenWrapperDownloader
      */
     private static final String PROPERTY_NAME_WRAPPER_URL = "wrapperUrl";
 
-    public static void main( String args[] )
+    public static void main( String[] args )
     {
-        System.out.println( "- Downloader started" );
-        File baseDirectory = new File( args[0] );
-        System.out.println( "- Using base directory: " + baseDirectory.getAbsolutePath() );
+        if ( args.length == 0 )
+        {
+            System.err.println( " - ERROR projectBasedir parameter missing" );
+            System.exit( 1 );
+        }
+
+        log( " - Downloader started" );
+        final String dir = args[0].replace( "..", "" ); // Sanitize path
+        final Path projectBasedir = Paths.get( dir ).toAbsolutePath().normalize();
+        if ( !Files.isDirectory( projectBasedir, LinkOption.NOFOLLOW_LINKS ) )
+        {
+            System.err.println( " - ERROR projectBasedir not exists: " + projectBasedir );
+            System.exit( 1 );
+        }
+
+        log( " - Using base directory: " + projectBasedir );
 
         // If the maven-wrapper.properties exists, read it and check if it contains a custom
         // wrapperUrl parameter.
-        File mavenWrapperPropertyFile = new File( baseDirectory, MAVEN_WRAPPER_PROPERTIES_PATH );
-        String url = DEFAULT_DOWNLOAD_URL;
-        if ( mavenWrapperPropertyFile.exists() )
-        {
-            FileInputStream mavenWrapperPropertyFileInputStream = null;
-            try
-            {
-                mavenWrapperPropertyFileInputStream = new FileInputStream( mavenWrapperPropertyFile );
-                Properties mavenWrapperProperties = new Properties();
-                mavenWrapperProperties.load( mavenWrapperPropertyFileInputStream );
-                url = mavenWrapperProperties.getProperty( PROPERTY_NAME_WRAPPER_URL, url );
-            }
-            catch ( IOException e )
-            {
-                System.out.println( "- ERROR loading '" + MAVEN_WRAPPER_PROPERTIES_PATH + "'" );
-            }
-            finally
-            {
-                try
-                {
-                    if ( mavenWrapperPropertyFileInputStream != null )
-                    {
-                        mavenWrapperPropertyFileInputStream.close();
-                    }
-                }
-                catch ( IOException e )
-                {
-                    // Ignore ...
-                }
-            }
-        }
-        System.out.println( "- Downloading from: " + url );
+        Path mavenWrapperPropertyFile = projectBasedir.resolve( MAVEN_WRAPPER_PROPERTIES_PATH );
+        String url = readWrapperUrl( mavenWrapperPropertyFile );
 
-        File outputFile = new File( baseDirectory.getAbsolutePath(), MAVEN_WRAPPER_JAR_PATH );
-        if ( !outputFile.getParentFile().exists() )
-        {
-            if ( !outputFile.getParentFile().mkdirs() )
-            {
-                System.out.println( "- ERROR creating output directory '" + outputFile.getParentFile().getAbsolutePath()
-                    + "'" );
-            }
-        }
-        System.out.println( "- Downloading to: " + outputFile.getAbsolutePath() );
         try
         {
+            Path outputFile = projectBasedir.resolve( MAVEN_WRAPPER_JAR_PATH );
+            createDirectories( outputFile.getParent() );
             downloadFileFromURL( url, outputFile );
-            System.out.println( "Done" );
+            log( "Done" );
             System.exit( 0 );
         }
-        catch ( Throwable e )
+        catch ( IOException e )
         {
-            System.out.println( "- Error downloading" );
+            System.err.println( "- Error downloading" );
             e.printStackTrace();
             System.exit( 1 );
         }
     }
 
-    private static void downloadFileFromURL( String urlString, File destination )
-        throws Exception
+    private static void downloadFileFromURL( String urlString, Path destination ) throws IOException
     {
+        log( " - Downloading to: " + destination );
         if ( System.getenv( "MVNW_USERNAME" ) != null && System.getenv( "MVNW_PASSWORD" ) != null )
         {
-            String username = System.getenv( "MVNW_USERNAME" );
-            char[] password = System.getenv( "MVNW_PASSWORD" ).toCharArray();
+            final String username = System.getenv( "MVNW_USERNAME" );
+            final char[] password = System.getenv( "MVNW_PASSWORD" ).toCharArray();
             Authenticator.setDefault( new Authenticator()
             {
                 @Override
@@ -131,12 +116,47 @@ public class MavenWrapperDownloader
             } );
         }
         URL website = new URL( urlString );
-        ReadableByteChannel rbc;
-        rbc = Channels.newChannel( website.openStream() );
-        FileOutputStream fos = new FileOutputStream( destination );
-        fos.getChannel().transferFrom( rbc, 0, Long.MAX_VALUE );
-        fos.close();
-        rbc.close();
+        try ( InputStream inStream = website.openStream() ) {
+            Files.copy( inStream, destination, StandardCopyOption.REPLACE_EXISTING );
+        }
+        log( " - Downloader complete" );
+    }
+
+    private static void createDirectories(Path outputPath) throws IOException
+    {
+        if ( !Files.isDirectory( outputPath, LinkOption.NOFOLLOW_LINKS ) ) {
+            Path createDirectories = Files.createDirectories( outputPath );
+            log( " - Directories created: " + createDirectories );
+        }
+    }
+
+    private static String readWrapperUrl( Path mavenWrapperPropertyFile )
+    {
+        String url = DEFAULT_DOWNLOAD_URL;
+        if ( Files.exists( mavenWrapperPropertyFile, LinkOption.NOFOLLOW_LINKS ) )
+        {
+            log( " - Reading property file: " + mavenWrapperPropertyFile );
+            try ( InputStream in = Files.newInputStream( mavenWrapperPropertyFile, StandardOpenOption.READ ) )
+            {
+                Properties mavenWrapperProperties = new Properties();
+                mavenWrapperProperties.load( in );
+                url = mavenWrapperProperties.getProperty( PROPERTY_NAME_WRAPPER_URL, DEFAULT_DOWNLOAD_URL );
+            }
+            catch ( IOException e )
+            {
+                System.err.println( " - ERROR loading '" + MAVEN_WRAPPER_PROPERTIES_PATH + "'" );
+            }
+        }
+        log( " - Downloading from: " + url );
+        return url;
+    }
+
+    private static void log( String msg )
+    {
+        if ( VERBOSE )
+        {
+            System.out.println( msg );
+        }
     }
 
 }
