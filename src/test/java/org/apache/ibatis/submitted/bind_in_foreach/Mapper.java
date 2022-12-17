17a99302f2f6276f745723f08fa83087f335b2c9
Iwao AVE! <harawata@gmail.com>
2022-12-17 12:39:38 +0900

    Added a test case for #2754
    Also related to #206 #575

---
+++ src/test/java/org/apache/ibatis/submitted/bind_in_foreach/Mapper.java
@@ -0,0 +1,29 @@
+/*
+ *    Copyright 2009-2022 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.submitted.bind_in_foreach;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface Mapper {
+
+  @Select("select * from users order by id")
+  List<User> selectUsers();
+
+  int createUsers(List<Integer> numbers);
+
+}
