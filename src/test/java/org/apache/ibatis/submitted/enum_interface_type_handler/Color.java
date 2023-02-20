57d10a687c13d7e5909d5fa4db795620ef736125
Jeremy Landis <jeremylandis@hotmail.com>
2023-02-20 18:27:51 -0500

    [ci] Add missing overrides

--- src/test/java/org/apache/ibatis/submitted/enum_interface_type_handler/Color.java
+++ src/test/java/org/apache/ibatis/submitted/enum_interface_type_handler/Color.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009-2022 the original author or authors.
+ *    Copyright 2009-2023 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,16 +17,19 @@ package org.apache.ibatis.submitted.enum_interface_type_handler;
 
 public enum Color implements HasValue {
   WHITE {
+    @Override
     public int getValue() {
       return 1;
     }
   },
   RED {
+    @Override
     public int getValue() {
       return 2;
     }
   },
   BLUE {
+    @Override
     public int getValue() {
       return 3;
     }
