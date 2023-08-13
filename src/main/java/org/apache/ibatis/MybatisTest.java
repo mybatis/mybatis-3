0b4a1aa81f560021e326dc49c15ec8c6cf7f2a28
王建欢 <1151777592@qq.com>
2023-08-14 00:31:16 +0800

    update:mybatis的getMapper获取对应的mapper执行接口获取执行sql的逻辑梳理

--- src/main/java/org/apache/ibatis/MybatisTest.java
+++ src/main/java/org/apache/ibatis/MybatisTest.java
@@ -1,12 +1,13 @@
 package org.apache.ibatis;
 
+import org.apache.ibatis.entity.User;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapper.UserMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
-
 public class MybatisTest {
     public static void main(String[] args) throws Exception {
         InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
@@ -24,7 +25,18 @@ public class MybatisTest {
             sqlSession.close();
         }
         SqlSession sqlSession1 = sqlSessionFactory.openSession();
-        Object o1 = sqlSession1.selectOne("selectById", 1);
-        System.out.println("one = " + o1);
+        try {
+            Object o1 = sqlSession1.selectOne("selectById", 1);
+            System.out.println("one = " + o1);
+        } finally {
+            sqlSession1.close();
+        }
+        SqlSession sqlSession2 = sqlSessionFactory.openSession();
+        // 在xml解析的时候  通过namespace获取到对应的接口，然后保存到configuration中 可以通过getMapper方法获取到该mapper的代理对象(下面的语句对应为UserMapper其实是mapper的代理对象)
+        UserMapper mapper = sqlSession2.getMapper(UserMapper.class);
+        // mapper执行该mapper的代理对象其实执行MapperProxy里的invoke方法(mapperMethod.execute(sqlSession, args)),然后执行selectOne方法， 与上面的selectOne执行相同并获取对应的值。
+        User user = mapper.selectById(1);
+        System.out.println("user = " + user);
+
     }
 }
