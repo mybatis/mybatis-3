4201a8c4de4600e691c1819ca33469a58a0029f4
Jeremy Landis <jeremylandis@hotmail.com>
2023-11-02 19:21:22 -0400

    Removing the old site due to left over items and length issues

--- es/cobertura/js/popup.js
+++
@@ -1,8 +0,0 @@
-var newwindow;
-function popupwindow(url)
-{
-	newwindow=window.open(url,'name','height=500,width=500,resizable=yes,scrollbars=yes');
-	if (window.focus) {
-		newwindow.focus()
-	}
-}
--- ja/apidocs/tag-search-index.js
+++
@@ -1,1 +0,0 @@
-tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"},{"l":"Serialized Form","h":"","u":"serialized-form.html"}];updateSearchResults();
