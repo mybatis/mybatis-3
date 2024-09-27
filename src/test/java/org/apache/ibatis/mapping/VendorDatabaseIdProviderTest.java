12d45a015bdcf391baf034e855e2b361c4d024ba
Jeremy Landis <jeremylandis@hotmail.com>
2024-09-26 20:34:25 -0400

    [ci] Formatting

--- src/test/java/org/apache/ibatis/mapping/VendorDatabaseIdProviderTest.java
+++ src/test/java/org/apache/ibatis/mapping/VendorDatabaseIdProviderTest.java
@@ -13,7 +13,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package org.apache.ibatis.mapping;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/org/apache/ibatis/parsing/XNodeTest.java
+++ src/test/java/org/apache/ibatis/parsing/XNodeTest.java
@@ -13,7 +13,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package org.apache.ibatis.parsing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
