b9bdbd1937aee83c167bd57d3d2406d1a2f6cfe5
Iwao AVE! <harawata@gmail.com>
2024-11-20 05:39:21 +0900

    Modified 2 tests to ensure local cache behavior
    Local cache is used for nested select even if `localCacheScope` is `STATEMENT`.
    This should prevent regression caused by changes like #3299

--- src/test/java/org/apache/ibatis/submitted/permissions/PermissionsTest.java
+++ src/test/java/org/apache/ibatis/submitted/permissions/PermissionsTest.java
@@ -20,12 +20,15 @@ import java.util.List;
 
 import org.apache.ibatis.BaseDataTest;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.LocalCacheScope;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class PermissionsTest {
 
@@ -66,8 +69,10 @@ class PermissionsTest {
     }
   }
 
-  @Test
-  void checkNestedSelectLoop() {
+  @ParameterizedTest
+  @EnumSource
+  void checkNestedSelectLoop(LocalCacheScope localCacheScope) {
+    sqlSessionFactory.getConfiguration().setLocalCacheScope(localCacheScope);
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       final PermissionsMapper mapper = sqlSession.getMapper(PermissionsMapper.class);
 
@@ -93,6 +98,9 @@ class PermissionsTest {
       if (!readFound) {
         Assertions.fail();
       }
+    } finally {
+      // Reset the scope for other tests
+      sqlSessionFactory.getConfiguration().setLocalCacheScope(LocalCacheScope.SESSION);
     }
   }
 
