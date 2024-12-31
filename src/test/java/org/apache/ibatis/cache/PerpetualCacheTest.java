6662e779ed21f3f8962d3b0d97aae84b0a9e9ede
Jeremy Landis <jeremylandis@hotmail.com>
2024-12-30 23:39:48 -0500

    tests: Lambda cleanup

--- src/test/java/org/apache/ibatis/cache/PerpetualCacheTest.java
+++ src/test/java/org/apache/ibatis/cache/PerpetualCacheTest.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009-2023 the original author or authors.
+ *    Copyright 2009-2024 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -75,7 +75,7 @@ class PerpetualCacheTest {
   @Test
   void shouldDemonstrateIdIsNull() {
     Cache cache = new PerpetualCache(null);
-    assertThrows(CacheException.class, () -> cache.hashCode());
+    assertThrows(CacheException.class, cache::hashCode);
     assertThrows(CacheException.class, () -> cache.equals(new Object()));
   }
 }
