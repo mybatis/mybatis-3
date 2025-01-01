68bba510b51473de827bc55b80dfaab1abb29540
Iwao AVE! <harawata@gmail.com>
2025-01-01 18:25:51 +0900

    License years

--- src/main/java/org/apache/ibatis/binding/MapperProxy.java
+++ src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009-2024 the original author or authors.
+ *    Copyright 2009-2025 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -51,8 +51,7 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
       privateLookupInMethod = MethodHandles.class.getMethod("privateLookupIn", Class.class, MethodHandles.Lookup.class);
     } catch (NoSuchMethodException e) {
       throw new IllegalStateException(
-          "There is no 'privateLookupIn(Class, Lookup)' method in java.lang.invoke.MethodHandles.",
-          e);
+          "There is no 'privateLookupIn(Class, Lookup)' method in java.lang.invoke.MethodHandles.", e);
     }
   }
 
