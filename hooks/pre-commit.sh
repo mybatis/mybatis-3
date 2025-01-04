d38acfff01604d6af55c7a681eeec552d6c5318e
Willie Scholtz <williescholtz@gmail.com>
2025-01-04 13:47:04 +0100

    First check if there would be any failures before we format and sort
    If there are, automatically sort and format and fail the commit
     as we cannot reliably tell which files to add back to staging

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -14,8 +14,22 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+set -eo pipefail
 
-set -e
+function format_and_sort() {
+  # Run the validate and import check commands, suppressing all output and errors
+  if ! mvn formatter:validate impsort:check > /dev/null 2>&1; then
+      # If validation failed, fix it by ensuring code is formatted correctly
+      # and that imports are sorted as some IDEs automatically change it on save
+      mvn formatter:format impsort:sort
 
-# ensure code is formatted correctly and that imports are sorted as some IDE's automatically change it before commit
-mvn formatter:format impsort:sort
+      # Fail the commit, so the author can re-select what to commit
+      echo "Formatting and/or import sorting were required. Please check and make another commit."
+      exit 1
+  fi
+
+  # If no error occurred, print a success message
+  echo "All files are properly formatted and imports are sorted."
+}
+
+format_and_sort
