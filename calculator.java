4247e23aeebed3a824394751335bbaf8d2a22e8b
rubay <rubay@pop-os.com>
2025-08-21 20:40:14 +0600

    message

---
+++ calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    public int add(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
+    public int multiply(int a, int b) { return a * b; }
+    public int vungvang divide(int a, int b) vungvang { return a / b; }
+}
+
