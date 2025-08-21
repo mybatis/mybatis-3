b43b4bff196ac45203d2f45edcd4dc08c4173e6e
rubay <rubay@pop-os.com>
2025-08-21 20:58:49 +0600

    removed calculator from master

--- calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator {
-    public int add(int a, int b) { return a + b; }
-    public int subtract(int a, int b) { return a - b; }
-    public int multiply(int a, int b) { return a * b; }
-    public int divide(int a, int b) { return a / b; }
-}
-    public int divide(int a, int b) { return a / b; }
-}
-
